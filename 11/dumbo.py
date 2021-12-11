5f69094ba9aec0f30015682a7000e6ada0383500
Petr Viktorin <encukou@gmail.com>
2021-12-11 16:44:47 +0100

    Add graphical output

--- 11/dumbo.py
+++ 11/dumbo.py
@@ -1,5 +1,52 @@
 import numpy
 from scipy.signal import convolve2d
+import PIL.Image
+import PIL.ImageOps
+
+# Before running this:
+#    mkdir images
+# To render the video file:
+#    
+# (incantation taken from https://newbedev.com/ffmpeg-slideshow-with-crossfade)
+
+OCTOPUS_SIZE = 256
+BORDER = OCTOPUS_SIZE // 2
+main_image = PIL.Image.open('octopus.png')
+images = []
+for y in range(3):
+    for x in range(4):
+        images.append(PIL.ImageOps.expand(
+            main_image.crop((
+                x * OCTOPUS_SIZE,
+                y * OCTOPUS_SIZE,
+                (x+1) * OCTOPUS_SIZE,
+                (y+1) * OCTOPUS_SIZE,
+            )),
+            border=BORDER,
+            fill=(0, 0, 0, 0),
+        ))
+images[0] = main_image.crop((
+    2 * OCTOPUS_SIZE,
+    2 * OCTOPUS_SIZE,
+    4 * OCTOPUS_SIZE,
+    4 * OCTOPUS_SIZE,
+))
+
+
+def output_image(step_num, dumbos):
+    output = PIL.Image.new(
+        'RGBA',
+        (OCTOPUS_SIZE * dumbos.shape[1], OCTOPUS_SIZE * dumbos.shape[0]),
+        color=(0, 0, 0, 255),
+    )
+    for y, row in enumerate(dumbos):
+        for x, energy in enumerate(row):
+            output.alpha_composite(
+                images[energy],
+                dest=((OCTOPUS_SIZE * y - BORDER, OCTOPUS_SIZE * x - BORDER)),
+            )
+    output.save(f'images/step-{step_num}.png')
+
 
 dumbos = numpy.array([
     [int(c) for c in row.strip()]
@@ -18,7 +65,12 @@ dumbos = numpy.array([
 ])
 flash_window = numpy.ones((3, 3), dtype=int)
 
+
 total_flashes = 0
+first_all_flash = None
+all_flash_count = 0
+
+output_image(0, dumbos)
 
 for step_num in range(1, 101000000):
     dumbos += 1
@@ -33,10 +85,16 @@ for step_num in range(1, 101000000):
     total_flashes += flashed.sum()
     dumbos[dumbos > 9] = 0
 
-    print('step', step_num)
+    print(f'After step {step_num}, seen {total_flashes} flashes')
     print(dumbos)
 
-    if flashed.all():
-        break
+    output_image(step_num, dumbos.copy())
 
-print(total_flashes)
+    if flashed.all():
+        if first_all_flash is None:
+            first_all_flash = step_num
+        all_flash_count += 1
+        if all_flash_count > 10:
+            break
+
+print(f'All dumbos first flashed in step {first_all_flash}')
