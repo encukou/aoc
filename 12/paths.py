b38b62f472b65b4291938c26e503b790d706cbef
Petr Viktorin <encukou@gmail.com>
2021-12-12 17:54:13 +0100

    Day 12 solution (part 1)

---
+++ 12/paths.py
@@ -0,0 +1,45 @@
+
+maze = {}
+
+for line in """
+start-co
+ip-WE
+end-WE
+le-ls
+wt-zi
+end-sz
+wt-RI
+wt-sz
+zi-start
+wt-ip
+YT-sz
+RI-start
+le-end
+ip-sz
+WE-sz
+le-WE
+le-wt
+zi-ip
+RI-zi
+co-zi
+co-le
+WB-zi
+wt-WE
+co-RI
+RI-ip
+""".strip().splitlines():
+    a, b = line.strip().split('-')
+    maze.setdefault(a, []).append(b)
+    maze.setdefault(b, []).append(a)
+
+
+def num_paths(path_so_far, current_cave):
+    if current_cave == 'end':
+        return 1
+    count = 0
+    for next_cave in maze[current_cave]:
+        if next_cave not in path_so_far or next_cave.isupper():
+            count += num_paths(path_so_far + [current_cave], next_cave)
+    return count
+
+print(num_paths([], 'start'))
