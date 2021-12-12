c6d478d5781ecd63444d102f4da3173b60046138
Petr Viktorin <encukou@gmail.com>
2021-12-12 17:58:28 +0100

    Day 12, part 2

--- 12/paths.py
+++ 12/paths.py
@@ -33,13 +33,24 @@ RI-ip
     maze.setdefault(b, []).append(a)
 
 
-def num_paths(path_so_far, current_cave):
+def num_paths(path_so_far, current_cave, can_revisit_small=False):
     if current_cave == 'end':
         return 1
     count = 0
     for next_cave in maze[current_cave]:
         if next_cave not in path_so_far or next_cave.isupper():
-            count += num_paths(path_so_far + [current_cave], next_cave)
+            count += num_paths(
+                path_so_far + [current_cave],
+                next_cave,
+                can_revisit_small=can_revisit_small,
+            )
+        elif can_revisit_small and next_cave != 'start':
+            count += num_paths(
+                path_so_far + [current_cave],
+                next_cave,
+                can_revisit_small=False,
+            )
     return count
 
-print(num_paths([], 'start'))
+print(num_paths([], 'start', can_revisit_small=False))
+print(num_paths([], 'start', can_revisit_small=True))
