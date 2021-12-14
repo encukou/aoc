edddca322555dc625617122fa08a951f6e424efd
Petr Viktorin <encukou@gmail.com>
2021-12-14 09:15:12 +0100

    Day 14, part 2

--- 14/polymer.py
+++ 14/polymer.py
@@ -9,18 +9,25 @@ with open('input.txt') as input_file:
             pair, inserted = line.split(' -> ')
             rules[pair] = inserted
 
-for turn in range(1, 11):
-    print(f'Turn {turn}')
-    print(polymer)
-    next_polymer = []
-    for a, b in zip(polymer, polymer[1:]):
-        next_polymer.append(a)
-        if inserted := rules.get(a + b):
-            next_polymer.append(inserted)
-    next_polymer.append(polymer[-1])
-    polymer = next_polymer
+pairs = collections.Counter(list(zip(polymer, polymer[1:])))
+last_atom = polymer[-1]
 
-counts = collections.Counter(polymer)
+for turn in range(1, 41):
+    print(f'Turn {turn}')
+    print(pairs.most_common())
+    next_pairs = collections.Counter()
+    for (a, b), num in pairs.items():
+        if inserted := rules.get(a + b):
+            next_pairs[a + inserted] += num
+            next_pairs[inserted + b] += num
+        else:
+            next_pairs[a + b] += num
+    pairs = next_pairs
+
+counts = collections.Counter()
+for (a, b), num in pairs.items():
+    counts[a] += num
+counts[last_atom] += 1
 print(counts)
 most_common = counts.most_common()
 print(most_common[0][-1] - most_common[-1][-1])
