f55af3c3c3098d09ccef24d1c9747d9f5346a1b7
Petr Viktorin <encukou@gmail.com>
2021-12-15 20:15:55 +0100

    Day 2, part 2

--- 02/steer.py
+++ 02/steer.py
@@ -15,3 +15,21 @@ for direction in directions:
         case _:
             raise ValueError(command)
 print('Part 1:', position * depth)
+
+
+position = 0
+depth = 0
+aim = 0
+for direction in directions:
+    command, value = direction.split()
+    match command:
+        case "forward":
+            position += int(value)
+            depth += int(value) * aim
+        case "down":
+            aim += int(value)
+        case "up":
+            aim -= int(value)
+        case _:
+            raise ValueError(command)
+print('Part 2:', position * depth)
