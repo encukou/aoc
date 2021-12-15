6dfef9ec905fba359adad48079468cbb442f9ede
Petr Viktorin <encukou@gmail.com>
2021-12-15 19:55:42 +0100

    Day 15: Polish -- finish up the A* algorithm

--- 15/path.py
+++ 15/path.py
@@ -7,8 +7,9 @@ with open('input.txt') as input_file:
         [int(c) for c in line.strip()]
         for line in input_file
     ])
-cave = numpy.hstack([(cave - 1 + i) % 9 + 1 for i in range(5)])
-cave = numpy.vstack([(cave - 1 + i) % 9 + 1 for i in range(5)])
+if True:  # these enlarge the array for part 2:
+    cave = numpy.hstack([(cave - 1 + i) % 9 + 1 for i in range(5)])
+    cave = numpy.vstack([(cave - 1 + i) % 9 + 1 for i in range(5)])
 cave = numpy.pad(cave, 1, constant_values=-1)
 
 start = 1, 1
@@ -31,6 +32,8 @@ print_counter = 0
 
 while tasks:
     _estimate, pos = heapq.heappop(tasks)
+    if pos == goal:
+        break
     cost = best_costs[pos]
     for dx, dy in (-1, 0), (0, -1), (1, 0), (0, 1):
         next_pos = pos[0] + dx, pos[1] + dy
@@ -39,7 +42,9 @@ while tasks:
         next_cost = cost + cave[next_pos]
         if best_costs[next_pos] == -1 or best_costs[next_pos] > next_cost:
             best_costs[next_pos] = next_cost
-            heapq.heappush(tasks, (cheap_estimate(next_pos), next_pos))
+            heapq.heappush(
+                tasks, (next_cost + cheap_estimate(next_pos), next_pos),
+            )
 
     # Printing the arrays is slow, just do it every once in a while
     print_counter += 1
@@ -49,4 +54,4 @@ while tasks:
         print(len(tasks), 'paths in progress')
 
 print(best_costs)
-print(best_costs[-2, -2])
+print(best_costs[goal])
