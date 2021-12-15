77fd9eece23c57b1d222800de914a38207a9c0f6
Petr Viktorin <encukou@gmail.com>
2021-12-15 22:00:13 +0100

    Day 7, part 2

--- 07/cachalot.py
+++ 07/cachalot.py
@@ -12,4 +12,20 @@ for position in range(1, max_depth):
         best_position = position
         best_cost = cost
 
-print(best_position, best_cost)
+print('Part 1:', best_position, best_cost)
+
+costs = []
+cost = 0
+for i in range(max_depth+1):
+    cost += i
+    costs.append(cost)
+
+best_position = 0
+best_cost = sum(costs[d] for d in data)
+for position in range(1, max_depth):
+    cost = sum(costs[abs(d - position)] for d in data)
+    if cost < best_cost:
+        best_position = position
+        best_cost = cost
+
+print('Part 2:', best_position, best_cost)
