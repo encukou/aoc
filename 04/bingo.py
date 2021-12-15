0aaef3db3df44f8e8cb75fdec1c958789e84ad58
Petr Viktorin <encukou@gmail.com>
2021-12-15 21:23:19 +0100

    Day 4, part 2

--- 04/bingo.py
+++ 04/bingo.py
@@ -13,18 +13,27 @@ boards = numpy.array(boards)
 print(numbers)
 print(boards)
 
-marks = boards * 0
-for called_number in numbers:
-    marks[boards == called_number] = 1
-    print(marks)
-    for axis in 1, 2:
-        done_boards = marks.all(axis=axis).any(axis=1)
-        if done_boards.any():
-            winning_board_no = done_boards.argmax()
-            break
-    else:
-        continue
-    break
-winning_board = boards[winning_board_no]
-winning_marks = marks[winning_board_no]
-print('Part 1:', winning_board[winning_marks == 0].sum() * called_number)
+done_template = numpy.zeros(boards.shape[0], dtype=int)
+
+def solve(part1=True):
+    marks = boards * 0
+    for called_number in numbers:
+        marks[boards == called_number] = 1
+        done_boards = done_template.copy()
+        for axis in 1, 2:
+            done_boards[marks.all(axis=axis).any(axis=1)] = 1
+        if part1:
+            if done_boards.any():
+                winning_board_no = done_boards.argmax()
+                break
+        else:
+            if done_boards.all():
+                winning_board_no = prev_done_boards.argmin()
+                break
+        prev_done_boards = done_boards
+    winning_board = boards[winning_board_no]
+    winning_marks = marks[winning_board_no]
+    print(winning_board_no)
+    return winning_board[winning_marks == 0].sum() * called_number
+print('Part 1:', solve())
+print('Part 2:', solve(False))
