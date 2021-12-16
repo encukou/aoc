e4e2c7ce149fdf038af218fe5aaa5514e2ac5c83
Petr Viktorin <encukou@gmail.com>
2021-12-16 21:20:05 +0100

    Day 10, part 2

--- 10/chunks.py
+++ 10/chunks.py
@@ -1,26 +1,43 @@
+import statistics
+
 with open('data.txt') as file:
     chunks = [line.strip() for line in file]
 
 open_to_close = dict(['()', '[]', '<>', '{}'])
-scores = {
+corrupt_values = {
     ')': 3,
     ']': 57,
     '}': 1197,
     '>': 25137,
 }
+completion_values = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+}
 
-score = 0
+corrupt_score = 0
+completion_scores = []
+
 for chunk in chunks:
     stack = []
     print(chunk)
     for paren in chunk:
-        print(paren, stack)
         if paren in open_to_close:
             stack.append(paren)
         elif paren == open_to_close[stack[-1]]:
             stack.pop()
         else:
-            score += scores[paren]
+            corrupt_score += corrupt_values[paren]
             break
+    else:
+        completion_score = 0
+        for i, paren in enumerate(reversed(stack)):
+            completion_score *= 5
+            completion_score += completion_values[open_to_close[paren]]
+        completion_scores.append(completion_score)
 
-print('Part 1:', score)
+print('Part 1:', corrupt_score)
+print(completion_scores)
+print('Part 2:', statistics.median(completion_scores))
