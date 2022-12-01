4a52611b77232885597fef3bbbb749cc1f9d1b32
Petr Viktorin <encukou@gmail.com>
2022-12-01 19:40:37 +0100

    Add a template & run script

---
+++ 2022/template.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import os
+SMALLDATA = 'SMALLDATA' in os.environ
+
+data = """
+""".strip()
+if not SMALLDATA:
+    data = Path('input.txt').read_text()
+
+data = data.splitlines()
+print(data)
+
+
+
+
+
+part1 = ...
+print('part 1:', part1)
+if SMALLDATA:
+    assert part1 == ...
+
+
+
+
+part2 = ...
+print('part 2:', part2)
+if SMALLDATA:
+    assert part2 == ...
