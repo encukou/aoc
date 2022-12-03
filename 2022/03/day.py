789f6a69776a2be16aedaabd1cfefeccd7423613
Petr Viktorin <encukou@gmail.com>
2022-12-03 06:16:52 +0100

    2022 day 3 part 2

--- 2022/03/day.py
+++ 2022/03/day.py
@@ -14,13 +14,17 @@ for line in data:
     half1 = line[:len(line)//2]
     half2 = line[len(line)//2:]
     [common] = set(half1) & set(half2)
-    print(common)
+    print(common, get_score(common))
     score += get_score(common)
 
 
 print('*** part 1:', score)
 
+score = 0
+for l0, l1, l2 in zip(data[0::3], data[1::3], data[2::3]):
+    [common] = set(l0) & set(l1) & set(l2)
+    print(common, get_score(common))
+    score += get_score(common)
 
 
-
-print('*** part 2:', ...)
+print('*** part 2:', score)
