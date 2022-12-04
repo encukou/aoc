5d9cb7bae855d593ee22e9443611de68c9f683bc
Petr Viktorin <encukou@gmail.com>
2022-12-04 06:10:20 +0100

    2022 day 4 part 2

--- 2022/04/day.py
+++ 2022/04/day.py
@@ -1,23 +1,21 @@
 import sys
 
 data = sys.stdin.read().splitlines()
-print(data)
 
 def parse_range(x):
     start, stop = x.split('-')
     return set(range(int(start), int(stop)+1))
 
-count = 0
+count1 = 0
+count2 = 0
 for line in data:
     a, b = line.split(',')
     a  = parse_range(a)
     b  = parse_range(b)
     if a >= b or a <= b:
-        count += 1
+        count1 += 1
+    if a & b:
+        count2 += 1
 
-print('*** part 1:', count)
-
-
-
-
-print('*** part 2:', ...)
+print('*** part 1:', count1)
+print('*** part 2:', count2)
