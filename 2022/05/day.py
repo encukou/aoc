b2f8073f089ad32509de780f4cd465395f3325a7
Petr Viktorin <encukou@gmail.com>
2022-12-05 06:50:33 +0100

    2022 day 5: add comments & simplify a bit

--- 2022/05/day.py
+++ 2022/05/day.py
@@ -5,43 +5,50 @@ data = sys.stdin.read().splitlines()
 
 MOVE_RE = re.compile(r'move (\d+) from (\d+) to (\d+)')
 
+def format_stacks(stacks):
+    """Format stacks for nicer display"""
+    return ''.join('[' + ''.join(s) + ']' for s in stacks)
+
 def solve(part_num):
     stacks = []
-    moving = False
     for line in data:
-        if not moving:
-            if line.strip().startswith('1'):
-                moving = True
+        if not line.startswith('move'):
+            if not line.strip().startswith('1'):
+                # Build the stacks line by line (top to bottom)
+                # Letters apear in positions 1, 5, 9, ...
+                for stack_num, letter in enumerate(line[1::4]):
+                    if letter == ' ':
+                        continue
+                    while len(stacks) <= stack_num:
+                        stacks.append([])
+                    stacks[stack_num].append(letter)
+                    print(f"{letter} to {stack_num}: {format_stacks(stacks)}")
+            else:
+                # Stacks are built. Reverse them (bottom to top), so
+                # pop()/append(), which work on the end of the list, simulate
+                # the crane.
+                print('Reverse stacks')
                 for stack in stacks:
                     stack.reverse()
-                print([''.join(s) for s in stacks])
-                continue
-            for stack_num, letter in enumerate(line[1::4]):
-                if letter == ' ':
-                    continue
-                while len(stacks) <= stack_num:
-                    stacks.append([])
-                print([''.join(s) for s in stacks], stack_num, letter)
-                stacks[stack_num].append(letter)
+                print(format_stacks(stacks))
         elif line.strip():
+            # Parse 'move' line
             amt, src, dst = MOVE_RE.match(line).groups()
             amt = int(amt)
             src = int(src) - 1
             dst = int(dst) - 1
             print(f'move {amt} from {src} to {dst}')
             if part_num == 1:
+                # pop/append N times
                 for n in range(amt):
                     stacks[dst].append(stacks[src].pop())
             elif part_num == 2:
+                # move whole sublist at once
                 stacks[dst].extend(stacks[src][-amt:])
                 del stacks[src][-amt:]
-            else:
-                raise ValueError(part_num)
-            print([''.join(s) for s in stacks])
-    return stacks
+            print(format_stacks(stacks))
+    # Return only the top crate of each stack
+    return ''.join(s[-1] for s in stacks)
 
-stacks = solve(1)
-print('*** part 1:', ''.join(s[-1] for s in stacks))
-
-stacks = solve(2)
-print('*** part 2:', ''.join(s[-1] for s in stacks))
+print('*** part 1:', solve(1))
+print('*** part 2:', solve(2))
