fef9d7db8629bc704bedf982b13946c2ebf04ea7
Petr Viktorin <encukou@gmail.com>
2022-12-06 06:41:37 +0100

    2022 day 6: Use a trailing window to simplify a bit

--- 2022/06/day.py
+++ 2022/06/day.py
@@ -5,8 +5,9 @@ data = sys.stdin.read().strip()
 
 def solve(part_number, length):
     for i in range(len(data)):
-        if len(set(data[i:i+length])) == length:
-            print(f'*** part {part_number}:', i+length)
+        window = data[i-length:i]
+        if len(set(window)) == length:
+            print(f'*** part {part_number}:', i)
             break
 
 solve(1, 4)
