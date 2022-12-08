064d8b3e7a7bffa178cc76c526398cb8352d5554
Petr Viktorin <encukou@gmail.com>
2022-12-08 06:18:04 +0100

    2022 day 8 part 2

--- 2022/08/day.py
+++ 2022/08/day.py
@@ -25,7 +25,31 @@ print(visible)
 
 print('*** part 1:', visible.sum())
 
+scenic_scores = numpy.zeros(trees.shape, dtype=int)
+for row_n, tree_row in enumerate(trees):
+    for col_n, tree in enumerate(tree_row):
+        scores = [0] * 4
+        for col in range(col_n+1, len(trees)):
+            scores[0] += 1
+            if trees[row_n, col] >= tree:
+                break
+        for col in range(col_n-1, -1, -1):
+            scores[1] += 1
+            if trees[row_n, col] >= tree:
+                break
+        for row in range(row_n+1, len(trees[0])):
+            scores[2] += 1
+            if trees[row, col_n] >= tree:
+                break
+        for row in range(row_n-1, -1, -1):
+            scores[3] += 1
+            if trees[row, col_n] >= tree:
+                break
+        score = scores[0]*scores[1]*scores[2]*scores[3]
+        print(row_n, col_n, tree, score)
+        scenic_scores[row_n, col_n] = score
+        
+print(scenic_scores)
 
 
-
-print('*** part 2:', ...)
+print('*** part 2:', scenic_scores.max())
