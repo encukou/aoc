67492f1f82b26f9388d6d916e079259e8f7f2f67
Petr Viktorin <encukou@gmail.com>
2022-12-09 06:58:29 +0100

    2022 day 9: Simplify

--- 2022/09/day.py
+++ 2022/09/day.py
@@ -3,61 +3,53 @@ import sys
 data = [d.strip().splitlines() for d in sys.stdin.read().split('---')]
 print(data)
 
-def sgn(n):
-    if n < 0:
-        return -1
-    elif n > 0:
-        return 1
-    else:
-        return 0
+def abs_sgn(n):
+    """Return the absolute value and sign (as -1, 1 or 0) of n
 
-def solve(rope_length, data_index):
+    Sign is 1 (positive), -1 (negative), or 0, see:
+    https://en.wikipedia.org/wiki/Sign_function
+    """
+    if n > 0:
+        return n, 1
+    if n < 0:
+        return -n, -1
+    return 0, 0
+
+def solve(rope_length, instructions):
     rope = [(0, 0)] * rope_length
     tail_positions = set()
-    for line in data[data_index]:
+    for line in instructions:
         print(line)
         direction, count = line.split()
         for i in range(int(count)):
+            # Move the "head" (first knot)
+            # `r` means row, `c` means column
             head_r, head_c = rope[0]
             match direction:
-                case 'R':
-                    head_c +=1
-                case 'L':
-                    head_c -=1
-                case 'U':
-                    head_r -=1
-                case 'D':
-                    head_r +=1
+                case 'R': head_c +=1
+                case 'L': head_c -=1
+                case 'D': head_r +=1
+                case 'U': head_r -=1
                 case _:
                     raise ValueError(direction)
             rope[0] = head_r, head_c
+            # Move the rest of the rope
             for n, _ in enumerate(rope[:-1]):
-                head_r, head_c = rope[n]
-                tail_r, tail_c = rope[n+1]
-                # more than 2 away in same row/column
-                if tail_r < head_r-1 and tail_c == head_c:
-                    tail_r += 1
-                elif tail_r > head_r+1 and tail_c == head_c:
-                    tail_r -= 1
-                elif tail_c < head_c-1 and tail_r == head_r:
-                    tail_c += 1
-                elif tail_c > head_c+1 and tail_r == head_r:
-                    tail_c -= 1
-                elif (
-                    # not in the same row/column
-                    tail_c != head_c and tail_r != head_r
-                    # not touching
-                    and abs(tail_c - head_c) > 1
-                    or abs(tail_r - head_r) > 1
-                ):
-                    tail_r += sgn(head_r - tail_r)
-                    tail_c += sgn(head_c - tail_c)
-                rope[n+1] = tail_r, tail_c
+                leader_r, leader_c = rope[n]
+                follower_r, follower_c = rope[n+1]
+                # Get distance and direction for each axis
+                dist_r, dir_r = abs_sgn(leader_r - follower_r)
+                dist_c, dir_c = abs_sgn(leader_c - follower_c)
+                # Move "follower" knot if it's more than 2 spots away
+                if max(dist_r, dist_c) >= 2:
+                    follower_r += dir_r
+                    follower_c += dir_c
+                rope[n+1] = follower_r, follower_c
+            # Record the tail (last knot) position
             tail_positions.add(rope[-1])
             print(rope)
-    print(tail_positions)
     return len(tail_positions)
 
 
-print('*** part 1:', solve(2, 0))
-print('*** part 2:', solve(10, -1))
+print('*** part 1:', solve(2, data[0]))
+print('*** part 2:', solve(10, data[-1]))
