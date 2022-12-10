fd4b9496d5bb33d665ebf76ac21365178a33df28
Petr Viktorin <encukou@gmail.com>
2022-12-10 06:30:46 +0100

    2022 day 10 part 2

--- 2022/10/day.py
+++ 2022/10/day.py
@@ -7,7 +7,6 @@ def scan(data):
     counter = 0
     register = 1
     for line in data:
-        print(counter, register, line)
         match line.split():
             case ['noop']:
                 counter += 1
@@ -22,11 +21,12 @@ def scan(data):
 def solve(data, interesting_times):
     total = 0
     for counter, register in scan(data):
+        print(counter, register)
         if counter in interesting_times:
             total += counter * register
     return total
 
-print('small:', solve(
+print('tiny:', solve(
     ['noop', 'addx 3', 'addx -5', 'noop', 'noop'],
     range(6),
 ))
@@ -35,5 +35,17 @@ print('small:', solve(
 
 print('*** part 1:', solve(data, [20, 60, 100, 140, 180, 220]))
 
+def draw(data):
+    display = [['.'] * 40 for x in range(6)]
+    for counter, register in scan(data):
+        row, col = divmod(counter-1, 40)
+        row = row % 6
+        if abs(col - (register)) < 2:
+            display[row][col] = '█'
+        print(f'{counter:3}, {register:2}, {row}, {"".join(display[row])}')
 
-print('*** part 2:', ...)
+    print()
+    for row, d in enumerate(display):
+        print(f'{row}, {"".join(d)}')
+
+draw(data)
