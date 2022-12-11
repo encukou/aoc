94361c1b77ae6d18dea591fd36bb4431dada3a40
Petr Viktorin <encukou@gmail.com>
2022-12-11 07:41:30 +0100

    2022 day 11: combine solutions

--- 2022/11/day.py
+++ 2022/11/day.py
@@ -14,51 +14,52 @@ class Monkey:
     dests: dict = field(default_factory=dict)
     activity: int = 0
 
-def val_or_old(x):
-    if x == 'old':
-        return x
-    return Value.from_int(int(x))
-
 PRIMES = (2, 3, 5, 7, 11, 13, 17, 19, 23)
 
 class Value:
+    """Acts as a number, but only stores remaiders of division by small primes
+
+    (Only implements the needed operations: addition & multiplication)
+    """
     def __init__(self, *, mods):
         self.mods = mods
 
     @classmethod
     def from_int(cls, n):
+        n = int(n)
         return cls(mods = {p: n % p for p in PRIMES})
 
     def __repr__(self):
-        return f'<{tuple(self.mods.values())}>'
+        return f'<{",".join(str(m) for m in self.mods.values())}>'
 
-    def add(self, other):
-        assert isinstance(self, Value)
-        assert isinstance(other, Value)
+    def __add__(self, other):
         return Value(mods={
             p: (self.mods[p] + other.mods[p]) % p for p in PRIMES
         })
 
-    def mul(self, other):
-        assert isinstance(self, Value)
-        assert isinstance(other, Value)
+    def __mul__(self, other):
         return Value(mods={
             p: (self.mods[p] * other.mods[p]) % p for p in PRIMES
         })
 
-    def div(self, other):
-        assert isinstance(self, Value)
-        assert isinstance(other, Value)
-        return Value(mods={
-            p: (self.mods[p] // other.mods[p]) % p for p in PRIMES
-        })
+    def is_divisible_by(self, n):
+        return not self.mods[n]
 
 OPS = {
-    '+': Value.add,
-    '*': Value.mul,
+    '+': operator.add,
+    '*': operator.mul,
 }
 
 def solve(num_rounds, divide_by_3):
+    if divide_by_3:
+        # Use regular integers
+        make_val = int
+        is_divisible_by = lambda a, b: not a % b
+    else:
+        # Use custom numbers
+        make_val = Value.from_int
+        is_divisible_by = Value.is_divisible_by
+
     monkeys = []
     for line in data:
         match line.split():
@@ -67,9 +68,13 @@ def solve(num_rounds, divide_by_3):
                 assert len(monkeys) == num
                 monkeys.append(Monkey())
             case 'Starting', 'items:', *rest:
-                monkeys[-1].items.extend(Value.from_int(int(i.strip(','))) for i in rest)
+                monkeys[-1].items.extend(make_val(i.strip(',')) for i in rest)
             case 'Operation:', 'new', '=', a, op, b:
-                monkeys[-1].operation = val_or_old(a), OPS[op], val_or_old(b)
+                if a != 'old':
+                    a = make_val(a)
+                if b != 'old':
+                    b = make_val(b)
+                monkeys[-1].operation = a, OPS[op], b
             case 'Test:', 'divisible', 'by', num:
                 monkeys[-1].test = int(num)
             case 'If', 'true:', 'throw', 'to', 'monkey', num:
@@ -85,9 +90,16 @@ def solve(num_rounds, divide_by_3):
         print(monkey)
 
     for round_num in range(num_rounds):
-        print(f'{round_num=}')
+        if round_num < 20 or not ((round_num-1) % 1000):
+            # Print out everyting!
+            log = print
+        else:
+            # Don't print out anything!
+            log = lambda *a: None
+
+        log(f'{round_num=}')
         for i, monkey in enumerate(monkeys):
-            #print(f'  {i}; {monkey.items=}')
+            log(f'  {i}; {monkey.items=}')
             while monkey.items:
                 old = monkey.items.popleft()
                 monkey.activity += 1
@@ -96,21 +108,20 @@ def solve(num_rounds, divide_by_3):
                     {'old': old}.get(a, a),
                     {'old': old}.get(b, b),
                 )
-                #print(f'    {old=} new={level} o={monkey.operation}')
-                #print(level, op)
+                log(f'    {old=} new={level} o={monkey.operation}')
+                log(level, op)
                 if divide_by_3:
                     level = level // 3
-                decision = not level.mods[monkey.test]
+                decision = is_divisible_by(level, monkey.test)
                 dest = monkey.dests[decision]
-                #print(f'    {level=} {decision=} {dest=}')
+                log(f'    {level=} {decision=} {dest=}')
                 monkeys[dest].items.append(level)
         for i, monkey in enumerate(monkeys):
-            print(i, monkey.activity, monkey.items)
+            log(i, monkey.activity, monkey.items)
 
     activities = sorted(monkey.activity for monkey in monkeys)
     return activities[-1] * activities[-2]
 
-print('*** part 1:', '10605')  # XXX
-
+print('*** part 1:', solve(20, divide_by_3=True))
 
 print('*** part 2:', solve(10_000, divide_by_3=False))
