b4a45317a121e1bea47e0025b2aafbaf577af373
Petr Viktorin <encukou@gmail.com>
2022-12-12 06:49:44 +0100

    2022 day 12: Clean up

--- 2022/12/day.py
+++ 2022/12/day.py
@@ -8,66 +8,59 @@ def to_num(c):
         return 26
     return ord(c) - ord('a')
 
-orig_data = numpy.array(
+data = numpy.array(
     [[to_num(c) for c in line] for line in sys.stdin.read().splitlines()]
 )
-data = numpy.pad(orig_data, 1, constant_values=99)
-print(data)
 
-steps = numpy.ones(data.shape, dtype=int) * 9999
-steps[data==-1] = 0
-
-def around(r, c):
+def neighborhood(r, c):
     yield r, c+1
     yield r, c-1
     yield r+1, c
     yield r-1, c
 
+# Setup
+elevations = numpy.pad(data, 1, constant_values=99)
+steps = numpy.ones(elevations.shape, dtype=int) * 9999
+[[start_r, start_c]] = numpy.argwhere(elevations == -1)
+steps[start_r, start_c] = 0
+to_visit = {(-1, 0, start_r, start_c)}
+[[goal_r, goal_c]] = numpy.argwhere(elevations == 26)
+print(elevations)
 print(steps)
-to_visit = set((-1, int(r), int(c)) for r, c in around(*numpy.where(steps==0)))
-r, c = numpy.where(steps==0)
-to_visit = {(-1, 0, int(r), int(c))}
-goal_r, goal_c = numpy.where(data==26)
-print(to_visit, goal_r, goal_c)
+print(f'{to_visit=}, {goal_r=}, {goal_c=}')
 
 while to_visit:
+    print(f'{len(to_visit)=}')
     elevation, n_steps, r, c = to_visit.pop()
     n_steps += 1
-    for rr, cc in around(r, c):
-        if data[rr, cc] <= elevation + 1 and steps[rr, cc] > n_steps:
+    for rr, cc in neighborhood(r, c):
+        if elevations[rr, cc] <= elevation + 1 and steps[rr, cc] > n_steps:
             steps[rr, cc] = n_steps
-            to_visit.add((data[rr, cc], n_steps, rr, cc))
+            to_visit.add((elevations[rr, cc], n_steps, rr, cc))
             print(steps)
 
-[part1] = steps[goal_r, goal_c]
+part1 = steps[goal_r, goal_c]
 print('*** part 1:', 31)#part1)
 
 
-data = numpy.pad(orig_data, 1, constant_values=-9)
-steps = numpy.ones(data.shape, dtype=int) * 9999
-steps[data==26] = 0
-
-def around(r, c):
-    yield r, c+1
-    yield r, c-1
-    yield r+1, c
-    yield r-1, c
-
+# Reset + new setup
+elevations = numpy.pad(data, 1, constant_values=-99)
+steps = numpy.ones(elevations.shape, dtype=int) * 9999
+[[start_r, start_c]] = numpy.argwhere(elevations == 26)
+steps[start_r, start_c] = 0
+to_visit = {(26, 0, start_r, start_c)}
+print(elevations)
 print(steps)
-to_visit = set((-1, int(r), int(c)) for r, c in around(*numpy.where(steps==0)))
-r, c = numpy.where(steps==0)
-to_visit = {(data[int(r), int(c)], 0, int(r), int(c))}
-print(to_visit, goal_r, goal_c)
+print(f'{to_visit=}')
 
 while to_visit:
+    print(f'{len(to_visit)=}')
     elevation, n_steps, r, c = to_visit.pop()
     n_steps += 1
-    for rr, cc in around(r, c):
-        print(rr, cc, data[rr, cc], elevation)
-        if data[rr, cc] >= elevation - 1 and steps[rr, cc] > n_steps:
+    for rr, cc in neighborhood(r, c):
+        if elevations[rr, cc] >= elevation - 1 and steps[rr, cc] > n_steps:
             steps[rr, cc] = n_steps
-            to_visit.add((data[rr, cc], n_steps, rr, cc))
+            to_visit.add((elevations[rr, cc], n_steps, rr, cc))
             print(steps)
 
-
-print('*** part 2:', min(steps[data<=0]))
+print('*** part 2:', min(steps[elevations<=0]))
