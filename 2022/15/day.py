d193a3c124ff4f1a3af80f9d42e15458d2d71870
Petr Viktorin <encukou@gmail.com>
2022-12-15 06:45:15 +0100

    2022 dat 15 part 2 (more brute force than necessary)

--- 2022/15/day.py
+++ 2022/15/day.py
@@ -6,8 +6,10 @@ data = sys.stdin.read().splitlines()
 SMALL = (len(data) < 20)
 if SMALL:
     TARGET_ROW = 10
+    SEARCH_RANGE = range(0, 20+1)
 else:
     TARGET_ROW = 2000000
+    SEARCH_RANGE = range(0, 4000000+1)
 PRINT = SMALL
 
 ROW_RE = re.compile(
@@ -27,11 +29,13 @@ xs = [sx for sx, sy, bx, by, d in records]+[bx for sx, sy, bx, by, d in records]
 xrange = range(min(xs)-max(ds), max(xs)+1+max(ds))
 ys = [sy for sx, sy, bx, by, d in records]+[by for sx, sy, bx, by, d in records]
 yrange = range(min(ys)-max(ds), max(ys)+1+max(ds))
+xrange = yrange = SEARCH_RANGE
 
-if not SMALL:
-    yrange = range(TARGET_ROW-5, TARGET_ROW+5)
+#if not SMALL:
+#    yrange = range(TARGET_ROW-5, TARGET_ROW+5)
 
 totals = {}
+possible_positions = set()
 for y in yrange:
     sranges = []
     for sx, sy, bx, by, d in records:
@@ -40,7 +44,7 @@ for y in yrange:
             sranges.append(range(sx-d+yd, sx+d-yd+1))
     sranges.sort(key=lambda r: r.start)
 
-    print(len(sranges), end='>')
+    print(f'{y:7} {len(sranges)}', end='>')
     simplified = True
     while simplified:
         simplified = False
@@ -63,6 +67,10 @@ for y in yrange:
                 simplified = True
                 break
     print(len(sranges), end=' ')
+    if len(sranges) > 1:
+        a, b = sranges
+        assert a.stop+1 == b.start
+        possible_positions.add((a.stop, y))
 
     sensors_in_row = {sx for sx, sy, bx, by, d in records if sy == y}
     beacons_in_row = {bx for sx, sy, bx, by, d in records if by == y}
@@ -96,5 +104,7 @@ print('*** part 1:', totals[TARGET_ROW])
 
 
 
+print(possible_positions)
+[(x, y)] = possible_positions
 
-print('*** part 2:', ...)
+print('*** part 2:', x * 4000000 + y)
