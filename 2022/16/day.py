b27d6c420dfbbb77e83d798b0793f2addaabe16e
Petr Viktorin <encukou@gmail.com>
2022-12-16 07:59:39 +0100

    2022 day 16 part 1, computes now

--- 2022/16/day.py
+++ 2022/16/day.py
@@ -19,43 +19,83 @@ LINE_RE = re.compile(
 rooms = {}
 for line in data:
     name, rate, connections = LINE_RE.match(line).groups()
-    rooms[name] = Room(name, int(rate), connections.split(', '))
-pprint(rooms)
+    rooms[name] = Room(
+        name, int(rate), {c: 1 for c in connections.split(', ')}
+    )
 
+pprint(rooms)
+print()
+
+def gen_connections(room, visited=frozenset()):
+    visited |= {room.name}
+    for conn, dist in room.connections.items():
+        if conn in visited:
+            continue
+        if rooms[conn].rate == 0:
+            yield from gen_connections(rooms[conn], visited)
+        else:
+            yield conn, dist + len(visited) - 1
+
+for room in rooms.values():
+    room.connections = dict(sorted(
+        gen_connections(room),
+        key=lambda r_d: -r_d[1],
+    ))
+rooms = {
+    room.name: room for room in rooms.values()
+    if room.rate or room.name =='AA'
+}
+
+pprint(rooms)
 max_rate = sum(r.rate for r in rooms.values())
+print('max_rate=', max_rate)
 
 @dataclass(unsafe_hash=True)
 class Node:
     flowed: int
     rate: int
     minute: int
+    heuristic: int = field(default=None, init=False)
     current: str
     to_open: frozenset
-    history: tuple = field(default=(), repr=False, hash=False)
+    history: tuple = field(default=(), repr=False, hash=False, compare=False)
 
     def __post_init__(self):
-        self.heuristic = self.flowed + (30-self.minute) * max_rate
+        self.heuristic = self.flowed + (40-self.minute) * max_rate
 
     def __lt__(self, other):
         return self.heuristic > other.heuristic
 
 visited = set()
 to_visit = [
-    Node(0, 0, 1, 'AA', frozenset(r for r in rooms if rooms[r].rate))
+    Node(0, 0, 1, 'AA', frozenset(rooms))
 ]
 best = None
 n = 0
 while to_visit:
     node = heappop(to_visit)
+    if node in visited:
+        continue
+    visited.add(node)
+    if node.minute > 30:
+        overshoot = node.minute - 30
+        heappush(to_visit, Node(
+            node.flowed - node.rate * overshoot,
+            new_rate,
+            node.minute - overshoot,
+            node.current,
+            node.to_open,
+            node.history,
+        ))
+        continue
     if node.minute == 30:
-        if best is None or node.heuristic > best.heuristic:
+        if best is None or node.flowed > best.flowed:
             best = node
         continue
     if best and node.heuristic < best.heuristic:
         continue
-    visited.add(node)
     n += 1
-    if n % 1000 == 0:
+    if n % 10000 == 0:
         print(node, flush=True)
     if node.current in node.to_open:
         new_rate = node.rate + rooms[node.current].rate
@@ -67,11 +107,11 @@ while to_visit:
             node.to_open - {node.current},
             node.history + (node,),
         ))
-    for conn in rooms[node.current].connections:
+    for conn, dist in rooms[node.current].connections.items():
         heappush(to_visit, Node(
-            node.flowed + node.rate,
+            node.flowed + node.rate * dist,
             node.rate,
-            node.minute + 1,
+            node.minute + dist,
             conn,
             node.to_open,
             node.history + (node,),
@@ -82,8 +122,6 @@ for n in best.history:
 print(best)
 
 print('*** part 1:', best.flowed)
-
-
 
 
 print('*** part 2:', ...)
