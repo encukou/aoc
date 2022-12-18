515abf611f52f017c1bd3f830f22ef1479de9245
Petr Viktorin <encukou@gmail.com>
2022-12-18 06:50:10 +0100

    2022 day 18: combine solutions

--- 2022/18/day.py
+++ 2022/18/day.py
@@ -1,5 +1,6 @@
 import sys
 import collections
+
 import numpy
 
 data = sys.stdin.read().splitlines()
@@ -15,37 +16,53 @@ def get_cube_faces(x, y, z):
     yield frozenset([(x, y, z), (x+1, y, z), (x+1, y, z+1), (x, y, z+1)])
     yield frozenset([(x, y+1, z), (x+1, y+1, z), (x+1, y+1, z+1), (x, y+1, z+1)])
 
-face_counter = collections.Counter()
-for line in data:
-    for face in get_cube_faces(*(int(c) for c in line.split(','))):
-        face_counter[face] += 1
+def count_boundary_faces(cubes):
+    face_counter = collections.Counter()
+    for cube in list(cubes):
+        for face in get_cube_faces(*cube):
+            face_counter[face] += 1
 
-num_singles = 0
-for face, count in face_counter.items():
-    print(face, count)
-    if count == 1:
-        num_singles += 1
+    # Only count faces that aren't duplicated
+    num_singles = 0
+    for face, count in face_counter.items():
+        if count == 1:
+            num_singles += 1
+    return num_singles
 
-print('*** part 1:', num_singles)
+cubes = [
+    tuple(int(c) for c in line.split(','))
+    for line in data
+]
+num_boundary_faces = count_boundary_faces(cubes)
 
-numpy.set_printoptions(threshold=10000)
-cubes = [tuple(int(c) for c in line.split(',')) for line in data]
-assert (0, 0, 0) not in cubes
+print('*** part 1:', num_boundary_faces)
+
+# Get a big enough NumPy array
 xs = [z for x, y, z in cubes]
-ys = [z for x, y, z in cubes]
+ys = [y for x, y, z in cubes]
 zs = [z for x, y, z in cubes]
-
 space = numpy.zeros((max(xs)+1, max(ys)+1, max(zs)+1), dtype=numpy.uint8)
+
+# The meaning of entries in the array:
+UNKNOWN = 0
+ROCK = 1
+PADDING = 2
+AIR = 3
+
 for cube in cubes:
-    space[cube] = 1
-space = numpy.pad(space, 1, constant_values=2)
+    space[cube] = ROCK
+
+# Pad the array with UNKNOWN and PADDING
+space = numpy.pad(space, 1, constant_values=UNKNOWN)
+space = numpy.pad(space, 1, constant_values=PADDING)
 print(space)
+
+# Flood fill UNKNOWN with AIR, starting in one corner
 frontier = {(1, 1, 1)}
 while frontier:
     cube = frontier.pop()
-    if space[cube] == 0:
-        space[cube] = 3
-        print(space)
+    if space[cube] == UNKNOWN:
+        space[cube] = AIR
         x, y, z = cube
         frontier.update((
             (x+1, y, z),
@@ -55,19 +72,13 @@ while frontier:
             (x, y, z+1),
             (x, y, z-1),
         ))
-print((space <= 1).nonzero())
-print(list(zip(*(space <= 1).nonzero())))
 
-face_counter = collections.Counter()
-for cube in list(zip(*(space <= 1).nonzero())):
-    for face in get_cube_faces(*cube):
-        face_counter[face] += 1
+# remaining UNKNOWN is ROCK
+space[space == UNKNOWN] = ROCK
+print(space)
 
-num_singles = 0
-for face, count in face_counter.items():
-    print(face, count)
-    if count == 1:
-        num_singles += 1
+# Process individual cubes as in part 1
+cubes = zip(*(space == ROCK).nonzero())
+num_boundary_faces = count_boundary_faces(cubes)
 
-print('*** part 2:', num_singles)
-# not 2056
+print('*** part 2:', num_boundary_faces)
