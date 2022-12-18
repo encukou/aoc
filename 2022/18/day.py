8e1eaac0498cc44c243746812023cce38f9e7e41
Petr Viktorin <encukou@gmail.com>
2022-12-18 06:35:46 +0100

    2022 day 18 part 2

--- 2022/18/day.py
+++ 2022/18/day.py
@@ -1,5 +1,6 @@
 import sys
 import collections
+import numpy
 
 data = sys.stdin.read().splitlines()
 
@@ -27,7 +28,46 @@ for face, count in face_counter.items():
 
 print('*** part 1:', num_singles)
 
+numpy.set_printoptions(threshold=10000)
+cubes = [tuple(int(c) for c in line.split(',')) for line in data]
+assert (0, 0, 0) not in cubes
+xs = [z for x, y, z in cubes]
+ys = [z for x, y, z in cubes]
+zs = [z for x, y, z in cubes]
 
+space = numpy.zeros((max(xs)+1, max(ys)+1, max(zs)+1), dtype=numpy.uint8)
+for cube in cubes:
+    space[cube] = 1
+space = numpy.pad(space, 1, constant_values=2)
+print(space)
+frontier = {(1, 1, 1)}
+while frontier:
+    cube = frontier.pop()
+    if space[cube] == 0:
+        space[cube] = 3
+        print(space)
+        x, y, z = cube
+        frontier.update((
+            (x+1, y, z),
+            (x-1, y, z),
+            (x, y+1, z),
+            (x, y-1, z),
+            (x, y, z+1),
+            (x, y, z-1),
+        ))
+print((space <= 1).nonzero())
+print(list(zip(*(space <= 1).nonzero())))
 
+face_counter = collections.Counter()
+for cube in list(zip(*(space <= 1).nonzero())):
+    for face in get_cube_faces(*cube):
+        face_counter[face] += 1
 
-print('*** part 2:', ...)
+num_singles = 0
+for face, count in face_counter.items():
+    print(face, count)
+    if count == 1:
+        num_singles += 1
+
+print('*** part 2:', num_singles)
+# not 2056
