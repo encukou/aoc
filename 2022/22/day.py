041b889d76bab0111a6ac799197103d1dac46d56
Petr Viktorin <encukou@gmail.com>
2022-12-23 00:29:01 +0100

    2022 day 22: Clean up

--- 2022/22/day.py
+++ 2022/22/day.py
@@ -9,6 +9,7 @@ from collections import namedtuple
 data = sys.stdin.read().splitlines()
 
 class Vec2D(namedtuple('_', ('r', 'c'))):
+    """A 2D vector (or point) with (row, column) coordinates"""
     @property
     def char(self):
         chars = {(0, 1): '>', (1, 0): 'v', (0, -1): '<', (-1, 0): '^'}
@@ -41,118 +42,114 @@ class BaseTile:
     def get_neighbor(self, facing):
         return self.map[self.pos + facing], facing
 
+class BaseMap(dict):
+    def __init__(self, data):
+        initial_tile = None
+        for row, line in enumerate(data, start=1):
+            if not line:
+                break
+            for col, char in enumerate(line, start=1):
+                if char != ' ':
+                    pos = Vec2D(row, col)
+                    self[pos] = t = self.tile_factory(
+                        pos=pos,
+                        char=char,
+                        map=self,
+                    )
+                    if initial_tile is None:
+                        self.initial_tile = initial_tile = t
+
+        rs = {r for r, c in self}
+        cs = {c for r, c in self}
+        self.ranges = range(min(rs), max(rs)+1), range(min(cs), max(cs)+1)
+
+    def draw(self, marks=()):
+        marks = dict(marks)
+        rrange, crange = self.ranges
+        rrange = range(rrange.start-1, rrange.stop)
+        crange = range(crange.start-1, crange.stop)
+        for dig in range(3):
+            print(f'    ', end=' ')
+            for c in crange:
+                print(format(c, '3')[dig], end=' ')
+            print()
+        print(end='   +-')
+        for c in crange:
+            print(end='--')
+        print()
+        for r in rrange:
+            print(f'{r:>3}|', end=' ')
+            for c in crange:
+                if mark := marks.get((r, c)):
+                    print(mark, end=' ')
+                elif t := self.get((r, c)):
+                    print(t.char, end=' ')
+                else:
+                    print(' ', end=' ')
+            print()
+
+    def solve(self):
+        facing = Vec2D(0, 1)
+        current_tile = self.initial_tile
+        journey = [(current_tile.pos, facing.char)]
+        self.draw(journey)
+        for instruction in re.split('(\d+)', data[-1]):
+            if not instruction:
+                continue
+            print('do', instruction)
+            try:
+                distance = int(instruction)
+            except ValueError:
+                facing = facing.turned(instruction)
+                journey.append((current_tile.pos, facing.char))
+            else:
+                for i in range(int(instruction)):
+                    new_tile, new_facing = current_tile.get_neighbor(facing)
+                    if new_tile.char == '#':
+                        break
+                    current_tile = new_tile
+                    facing = new_facing
+                    journey.append((current_tile.pos, facing.char))
+            if len(self) < 100:
+                self.draw(journey)
+
+        print('fin:')
+        self.draw(journey)
+        print(facing.char, facing)
+        r, c = current_tile.pos
+        return 1000 * r + 4 * c + '>v<^'.index(facing.char)
 
 @dataclass
-class Tile2D(BaseTile):
+class WraparoundTile(BaseTile):
+    """Tile for part 1: wraps around the map"""
     def get_neighbor(self, facing):
         try:
             return super().get_neighbor(facing)
         except KeyError:
             pass
         r, c = self.pos
-        dr, dc = facing
         rrange, crange = self.map.ranges
-        match dr, dc:
-            case 1, 0:
+        match facing.char:
+            case 'v':
                 r = rrange.start
-            case -1, 0:
+            case '^':
                 r = rrange.stop - 1
-            case 0, 1:
+            case '>':
                 c = crange.start
-            case 0, -1:
+            case '<':
                 c = crange.stop - 1
         pos = Vec2D(r, c)
         while pos not in self.map and c in crange and r in rrange:
             pos += facing
         return self.map[pos], facing
 
-class BaseMap(dict):
-    def initialize(self):
-        rs = {r for r, c in self}
-        cs = {c for r, c in self}
-        self.ranges = range(min(rs), max(rs)+1), range(min(cs), max(cs)+1)
+class WraparoundMap(BaseMap):
+    tile_factory = WraparoundTile
 
-class Map2D(BaseMap):
-    tile_factory = Tile2D
-
-def get_map(map_factory=Map2D):
-    result = map_factory()
-    initial_tile = None
-    for row, line in enumerate(data, start=1):
-        if not line:
-            break
-        for col, char in enumerate(line, start=1):
-            if char != ' ':
-                pos = Vec2D(row, col)
-                result[pos] = t = result.tile_factory(
-                    pos=pos,
-                    char=char,
-                    map=result,
-                )
-                if initial_tile is None:
-                    result.initial_tile = initial_tile = t
-    result.initialize()
-    return result
-
-def draw_map(m, marks, zoom=1):
-    marks = {(r//zoom*zoom, c//zoom*zoom): m for (r, c), m in dict(marks).items()}
-    rrange, crange = m.ranges
-    rrange = range(rrange.start-1, rrange.stop+zoom, zoom)
-    crange = range(crange.start-1, crange.stop+zoom, zoom)
-    for dig in range(3):
-        print(f'    ', end=' ')
-        for c in crange:
-            print(format(c, '3')[dig], end=' ')
-        print()
-    print(end='   +-')
-    for c in crange:
-        print(end='--')
-    print()
-    for r in rrange:
-        print(f'{r:>3}|', end=' ')
-        for c in crange:
-            if mark := marks.get((r, c)):
-                print(mark, end=' ')
-            elif t := m.get((r+zoom//2, c+zoom//2)):
-                print(t.char, end=' ')
-            else:
-                print(' ', end=' ')
-        print()
-
-def solve(the_map):
-    facing = Vec2D(0, 1)
-    current_tile = the_map.initial_tile
-    journey = [(current_tile.pos, facing.char)]
-    draw_map(the_map, journey)
-    for instruction in re.split('(\d+)', data[-1]):
-        if not instruction:
-            continue
-        print('do', instruction)
-        try:
-            distance = int(instruction)
-        except ValueError:
-            facing = facing.turned(instruction)
-            journey.append((current_tile.pos, facing.char))
-        else:
-            for i in range(int(instruction)):
-                new_tile, new_facing = current_tile.get_neighbor(facing)
-                if new_tile.char == '#':
-                    break
-                current_tile = new_tile
-                facing = new_facing
-                journey.append((current_tile.pos, facing.char))
-        if len(data) < 100:
-            draw_map(the_map, journey)
-
-    print('fin:')
-    draw_map(the_map, journey)
-    print(facing.char, facing)
-    r, c = current_tile.pos
-    return 1000 * r + 4 * c + '>v<^'.index(facing.char)
-
-print('*** part 1:', solve(get_map(Map2D)))
+print('*** part 1:', WraparoundMap(data).solve())
 
 class Vec3D(namedtuple('_', ('x', 'y', 'z'))):
+    """A 2D vector (or point) with (x, y, z) coordinates"""
     def __add__(self, other):
         sx, sy, sz = self
         ox, oy, oz = other
@@ -161,6 +158,9 @@ class Vec3D(namedtuple('_', ('x', 'y', 'z'))):
     def __neg__(self):
         x, y, z = self
         return Vec3D(-x, -y, -z)
+
+    def __sub__(self, other):
+        return self + -other
 
 X_AX = Vec3D(1, 0, 0)
 Y_AX = Vec3D(0, 1, 0)
@@ -171,20 +171,20 @@ class Tile3D(BaseTile):
         try:
             return super().get_neighbor(facing)
         except KeyError:
-            pass
-        pos3d, face = self.map.conv_2d_to_3d(self.pos)
-        facing3d = face.conv_facing_to_3d(facing)
-        pos3d += facing3d + -face.plane
-        new_face = self.map.faces[facing3d]
-        new_facing = new_face.conv_3d_to_facing(-face.plane)
-        new_pos = new_face.conv_3d_to_2d(pos3d)
-        return self.map[new_pos], new_facing
+            # Take a trip through 3D space!
+            pos3d, face = self.map.conv_2d_to_3d(self.pos)
+            facing3d = face.conv_facing_to_3d(facing)
+            pos3d += facing3d - face.plane
+            new_face = self.map.faces[facing3d]
+            new_facing = new_face.conv_3d_to_facing(-face.plane)
+            new_pos = new_face.conv_3d_to_2d(pos3d)
+            return self.map[new_pos], new_facing
 
 class Map3D(BaseMap):
     tile_factory = Tile3D
 
-    def initialize(self):
-        super().initialize()
+    def __init__(self, data):
+        super().__init__(data)
         self.cube_size = math.sqrt(len(self) / 6)
         assert self.cube_size.is_integer()
         self.cube_size = cs = int(self.cube_size)
@@ -288,6 +288,4 @@ class CubeFace:
             self.c_start + coords[self.c_axis],
         )
 
-map_3d = get_map(Map3D)
-
-print('*** part 2:', solve(map_3d))
+print('*** part 2:', Map3D(data).solve())
