6768776d6a6c1ca2086d5073931529413a781075
Petr Viktorin <encukou@gmail.com>
2022-12-23 06:53:43 +0100

    2022 day 23 part 2

--- 2022/23/day.py
+++ 2022/23/day.py
@@ -2,6 +2,7 @@ import sys
 from dataclasses import dataclass
 from collections import namedtuple, Counter
 from functools import cached_property
+from itertools import count
 
 data = sys.stdin.read().splitlines()
 print(data)
@@ -57,9 +58,9 @@ def print_elves(elves):
         for c in crange:
             if elf := elves.get((r, c)):
                 if elf == '#':
-                    print('#', end=' ')
+                    print('\x1b[7m# \x1b[27m', end='')
                 else:
-                    print('#', end=(elf-Vec2D(r,c)).char)
+                    print(f'\x1b[7m#{(elf-Vec2D(r,c)).char}\x1b[27m', end='')
             else:
                 print('. ', end='')
         print()
@@ -72,19 +73,25 @@ def load_elves(data):
                 elves[Vec2D(r, c)] = '#'
     return elves
 
-def simulate(data, n_steps):
+def simulate(data, n_steps=None):
     elves = load_elves(data)
     directions = list(CARDINAL_DIRECTIONS)
-    for step_i in range(n_steps):
-        print(f'go {directions[0]}')
+    if n_steps is None:
+        step_it = count()
+    else:
+        step_it = range(n_steps)
+    for step_i in step_it:
+        step_i += 1
         proposals = Counter()
         disp = {}
+        num_unhappy = 0
         for pos in elves:
             if not any(pos+a in elves for a in EIGHT_NEIBORHOOD):
                 proposed_pos = pos
                 elves[pos] = proposed_pos
                 proposals[proposed_pos] += 1
             else:
+                num_unhappy += 1
                 for d in directions:
                     if not any(pos+a in elves for a in d.adjacent_directions):
                         proposed_pos = pos+d
@@ -95,7 +102,10 @@ def simulate(data, n_steps):
                     proposed_pos = pos
                     elves[pos] = proposed_pos
                     proposals[proposed_pos] += 1
-        print_elves(elves)
+        print(f'{step_i}: {num_unhappy} elves wanna go, preferably to the {directions[0]}')
+        if step_i < 20 or step_i % 100 == 0:
+            print(end='', flush=True)
+            print_elves(elves)
         new_elves = {}
         for pos, prop in elves.items():
             if proposals[prop] == 1:
@@ -103,18 +113,20 @@ def simulate(data, n_steps):
             else:
                 new_elves[pos] = '#'
         elves = new_elves
+        if not num_unhappy:
+            break
         directions.append(directions.pop(0))
     print(f'fini:')
     print_elves(elves)
     rs = [r for r, c, in elves]
     cs = [c for r, c, in elves]
-    return (max(rs)-min(rs)+1) * (max(cs)-min(cs)+1) - len(elves)
+    empty_in_rect = (max(rs)-min(rs)+1) * (max(cs)-min(cs)+1) - len(elves)
+    return empty_in_rect, step_i
 
 simulate(WARMUP, 4)
 
-print('*** part 1:', simulate(data, 10))
+answer1, step_i = simulate(data, 10)
+print('*** part 1:', answer1)
 
-
-
-
-print('*** part 2:', ...)
+bignum, answer2 = simulate(data)
+print('*** part 2:', answer2)
