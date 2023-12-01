63d08d878a5afa670d6940df51d2b38a33a05d2e
Petr Viktorin <encukou@gmail.com>
2023-12-01 07:00:14 +0100

    2017 day 01 part 1

--- 2017/01/day.py
+++ 2017/01/day.py
@@ -1,11 +1,29 @@
 import sys
+import regex
 
-data = sys.stdin.read().splitlines()
+data = sys.stdin.read().strip()
 print(data)
 
+total = 0
+def solve(digits):
+    total = 0
+    for match in regex.finditer(
+        r'([0123456789])\1',
+        digits + digits[0],
+        overlapped=True,
+    ):
+        new = int(match[1])
+        total += new
+        print(new, '->', total)
+    return total
+
+assert solve('1122') == 3
+assert solve('1111') == 4
+assert solve('1234') == 0
+assert solve('91212129') == 9
 
 
-print('*** part 1:', ...)
+print('*** part 1:', solve(data))
 
 
 
