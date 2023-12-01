a8fd0a6b6fc53e0ecc2bf26244b68dad78697e70
Petr Viktorin <encukou@gmail.com>
2023-12-01 21:02:12 +0100

    2017 day 04 part 2

--- 2017/04/day.py
+++ 2017/04/day.py
@@ -15,6 +15,16 @@ assert valid('aa bb cc dd aaa')
 print('*** part 1:', sum(valid(row) for row in data))
 
 
+def valid2(phrase):
+    words = [''.join(sorted(w)) for w in phrase.split()]
+    return len(words) == len(set(words))
+
+assert valid2('abcde fghij')
+assert not valid2('abcde xyz ecdab')
+assert valid2('a ab abc abd abf abj')
+assert valid2('iiii oiii ooii oooi oooo')
+assert not valid2('oiii ioii iioi iiio')
 
 
-print('*** part 2:', ...)
+
+print('*** part 2:', sum(valid2(row) for row in data))
