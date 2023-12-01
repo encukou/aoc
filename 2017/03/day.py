920463e9c545479dc781b9113c7ec54a38dc457c
Petr Viktorin <encukou@gmail.com>
2023-12-01 20:55:33 +0100

    2017 day 03 part 2

--- 2017/03/day.py
+++ 2017/03/day.py
@@ -38,7 +38,25 @@ assert part1(1024) == 31
 
 print('*** part 1:', part1(data))
 
+CHECKS = {1:1, 2:1, 3:2, 4:4, 5:5}
+def part2():
+    memory = {(0, 0): 1}
+    for n, (r, c) in enumerate(generate_coords(), start=1):
+        new_val = sum((
+            memory.get((r+rr, c+cc), 0)
+            for rr, cc in (
+                (+1, 0), (-1, 0), (0, +1), (0, -1),
+                (+1, +1), (+1, -1), (-1, +1), (-1, -1),
+            )
+        ))
+        if n == 1:
+            new_val = 1
+        memory[r, c] = new_val
+        print(n, r, c, new_val, memory)
+        if check := CHECKS.get(n):
+            assert new_val == check
+        if new_val > data:
+            return new_val
 
 
-
-print('*** part 2:', ...)
+print('*** part 2:', part2())
