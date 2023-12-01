6ea62658e7aa734419c22eba1502f442ae09fb30
Petr Viktorin <encukou@gmail.com>
2023-12-01 07:04:46 +0100

    2017 day 01

--- 2017/01/day.py
+++ 2017/01/day.py
@@ -26,6 +26,21 @@ assert solve('91212129') == 9
 print('*** part 1:', solve(data))
 
 
+total = 0
+def part2(digits):
+    total = 0
+    for idx, digit in enumerate(digits):
+        if digit == digits[(idx + len(digits)//2) % len(digits)]:
+            new = int(digit)
+            total += new
+            print(new, '->', total)
+    return total
+
+assert part2('1212') == 6
+assert part2('1221') == 0
+assert part2('123425') == 4
+assert part2('123123') == 12
+assert part2('12131415') == 4
 
 
-print('*** part 2:', ...)
+print('*** part 2:', part2(data))
