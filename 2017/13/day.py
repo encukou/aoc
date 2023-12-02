73f838ac4df13dbc92b52a249340f699431c90d7
Petr Viktorin <encukou@gmail.com>
2023-12-02 13:30:21 +0100

    2017 day 13 part 2: this actually finishes

--- 2017/13/day.py
+++ 2017/13/day.py
@@ -9,54 +9,23 @@ for line in data:
     layer, range_ = line.split(':')
     layers[int(layer)] = int(range_)
 
-def draw(scanners, layers, location=-1):
-    max_depth = max(layers.values())
-    max_layer = max(layers)
-    for r in range(max_depth+1):
-        for c in range(max_layer+1):
-            sigil = ' '
-            if r == 0 and c == location:
-                paren = '()'
-            elif layers.get(c, -1) > r:
-                paren = '[]'
-            elif r == 0:
-                paren = '..'
-                sigil = '.'
-            else:
-                paren = '  '
-            if r == scanners.get(c):
-                sigil = 'S'
-            print(f'{paren[0]}{sigil}{paren[-1]} ', end='')
-        print()
-
 def solve(delay=0):
-    scanners = {depth: 0 for depth in layers}
-    scanner_directions = {depth: 1 for depth in layers}
-
-    print(delay, scanners, layers, scanner_directions)
-    #draw(scanners, layers)
-
+    print()
+    print(f'{delay=}')
     severity = crashes = 0
-    for location in range(-delay, max(layers)+1):
-        #print(f'Picosecond {location}: ({delay=})')
-        if location >= 0:
-            if scanners.get(location, -1) == 0:
+    for location in range(max(layers)+1):
+        if range_ := layers.get(location):
+            if (location+delay) % (2*range_-2) == 0:
                 new = location * layers[location]
                 severity += new
                 crashes += 1
-                print(f'***{new=} {severity=}**')
-                if delay:
-                    break
-            #draw(scanners, layers, location)
-        for scanner_id in scanners:
-            scanners[scanner_id] += scanner_directions[scanner_id]
-            if scanners[scanner_id] >= layers[scanner_id]:
-                scanners[scanner_id] -= 2
-                scanner_directions[scanner_id] = -1
-            if scanners[scanner_id] < 0:
-                scanners[scanner_id] += 2
-                scanner_directions[scanner_id] = +1
-        #draw(scanners, layers, location)
+                #print(f'Boom! @ {location=}! {new=} {severity=}')
+            else:
+                pass
+                #print((location+delay), (2*range_-2))
+        else:
+            pass
+            #print((location-delay), '-')
     return severity, crashes
 
 def get_severity():
