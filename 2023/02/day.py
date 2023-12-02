30729ea630e00e0b074e0fdcdd38ee293f10046c
Petr Viktorin <encukou@gmail.com>
2023-12-02 06:25:23 +0100

    2023 day 02:  separate the preprocessing

--- 2023/02/day.py
+++ 2023/02/day.py
@@ -6,52 +6,53 @@ import functools
 data = sys.stdin.read().splitlines()
 print(data)
 
+def gen_data():
+    for line in data:
+        game_id, draws = re.fullmatch(r'Game (\d+): (.*)', line).groups()
+        def parse_count_and_color(count, color):
+            return int(count), color
+        draws = [
+            [
+                parse_count_and_color(*count_and_color.split(' '))
+                for count_and_color in draw.split(', ')
+            ]
+            for draw in draws.split('; ')
+        ]
+        yield int(game_id), draws
+
 MAX = {
     'red': 12,
     'green': 13,
     'blue': 14,
 }
-print('have', MAX)
+print(f'{MAX=}')
 
 score = 0
-for line in data:
-    game_id, draws = re.fullmatch(r'Game (\d+): (.*)', line).groups()
-    game_id = int(game_id)
+for game_id, draws in gen_data():
     possible = True
-    for draw in draws.split('; '):
-        for colorcount in draw.split(', '):
-            num, color = colorcount.split(' ')
-            print(f'{game_id}: {num} {colorcount}')
-            if int(num) > MAX[color]:
+    for draw in draws:
+        for count, color in draw:
+            print(f'{game_id}: {count} {color}')
+            if count > MAX[color]:
                 print('-> impossible!')
                 possible = False
                 break
     if possible:
         score += game_id
-    print(f'... {score=}')
-
+    print(f'... {game_id=} {score=}')
 
 print('*** part 1:', score)
 
 
 score = 0
-for line in data:
-    fewest = {
-        'red': 0,
-        'green': 0,
-        'blue': 0,
-    }
-    game_id, draws = re.fullmatch(r'Game (\d+): (.*)', line).groups()
-    game_id = int(game_id)
-    for draw in draws.split('; '):
-        for colorcount in draw.split(', '):
-            num, color = colorcount.split(' ')
-            fewest[color] = max(fewest[color], int(num))
-            print(f'{game_id}: {num} {colorcount}. {fewest=}')
+for game_id, draws in gen_data():
+    fewest = {}
+    for draw in draws:
+        for count, color in draw:
+            fewest[color] = max(fewest.get(color, 0), count)
+            print(f'{game_id}: {count} {color}. {fewest=}')
     power = functools.reduce(operator.mul, fewest.values())
     score += power
-    print(f'... {power=} {score=}')
-
-
+    print(f'... {game_id=} {power=} {score=}')
 
 print('*** part 2:', score)
