bc0f03334e07a79c7d142227aba30b0a857fdc3a
Petr Viktorin <encukou@gmail.com>
2023-12-02 07:27:25 +0100

    2017 day 12 part 2

--- 2017/12/day.py
+++ 2017/12/day.py
@@ -10,18 +10,25 @@ for line in data:
 
 print(connections)
 
-group = set()
-to_add = {0}
-while to_add:
-    now = to_add.pop()
-    if now not in group:
-        group.add(now)
-        to_add.update(connections[now])
-        print(now, to_add, group)
+def get_group(seed=0):
+    group = set()
+    to_add = {seed}
+    while to_add:
+        now = to_add.pop()
+        if now not in group:
+            group.add(now)
+            to_add.update(connections[now])
+            print(now, to_add, group)
+    return group
 
-print('*** part 1:', len(group))
+print('*** part 1:', len(get_group()))
 
+remaining = set(connections)
+groups = []
+while remaining:
+    new_group = get_group(remaining.pop())
+    groups.append(new_group)
+    remaining.difference_update(new_group)
+    print(f'{remaining=}')
 
-
-
-print('*** part 2:', ...)
+print('*** part 2:', len(groups))
