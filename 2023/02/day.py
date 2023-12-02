08449dfd0bdc640611398a510490d3fa274aa9e8
Petr Viktorin <encukou@gmail.com>
2023-12-02 06:15:36 +0100

    2023 day 02 part 2

--- 2023/02/day.py
+++ 2023/02/day.py
@@ -1,5 +1,7 @@
 import sys
 import re
+import operator
+import functools
 
 data = sys.stdin.read().splitlines()
 print(data)
@@ -32,6 +34,24 @@ for line in data:
 print('*** part 1:', score)
 
 
+score = 0
+for line in data:
+    fewest = {
+        'red': 0,
+        'green': 0,
+        'blue': 0,
+    }
+    game_id, draws = re.fullmatch(r'Game (\d+): (.*)', line).groups()
+    game_id = int(game_id)
+    for draw in draws.split('; '):
+        for colorcount in draw.split(', '):
+            num, color = colorcount.split(' ')
+            fewest[color] = max(fewest[color], int(num))
+            print(f'{game_id}: {num} {colorcount}. {fewest=}')
+    power = functools.reduce(operator.mul, fewest.values())
+    score += power
+    print(f'... {power=} {score=}')
 
 
-print('*** part 2:', ...)
+
+print('*** part 2:', score)
