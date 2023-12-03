6f60bf33d49eb2e48da96213641fb7ec6d29bf38
Petr Viktorin <encukou@gmail.com>
2023-12-03 06:22:28 +0100

    2023 day 03 part 2

--- 2023/03/day.py
+++ 2023/03/day.py
@@ -1,5 +1,7 @@
 import sys
 import re
+import collections
+import math
 
 data = sys.stdin.read().splitlines()
 print(data)
@@ -9,23 +11,37 @@ SYMBOL_RE = re.compile(r'[^.\d]')
 
 lines = dict(enumerate(data))
 
+star_numbers = collections.defaultdict(list)
+
 total = 0
 for line_no, line in lines.items():
     for match in NUMBER_RE.finditer(line):
         print(match[0])
+        is_part = False
         for dn in (-1, 0, +1):
+            lookin_lineno = line_no+dn
             lookin_start = max(match.start()-1, 0)
-            lookin = lines.get(line_no+dn, '')[lookin_start:match.end()+1]
+            lookin_line = lines.get(lookin_lineno, '')
+            lookin = lookin_line[lookin_start:match.end()+1]
             sym_match = SYMBOL_RE.search(lookin)
-            print(f'{line_no+dn}. {lookin!r}: {bool(sym_match)}')
+            print(f'{line_no+dn:2}. {lookin!r}: {bool(sym_match)}')
             if sym_match:
-                total += int(match[0])
-                break
+                is_part = True
+                if sym_match[0] == '*':
+                    star_key = lookin_lineno, lookin_start+sym_match.start()
+                    star_numbers[star_key].append(int(match[0]))
+                    print(' *', star_key)
+        if is_part:
+            total += int(match[0])
 
 
 print('*** part 1:', total)
 
+part2 = 0
+for nums in star_numbers.values():
+    print(nums)
+    if len(nums) == 2:
+        a, b = nums
+        part2 += a * b
 
-
-
-print('*** part 2:', ...)
+print('*** part 2:', part2)
