9f72b9ddeaaaf8c578982f754e6f99014dfd4310
Petr Viktorin <encukou@gmail.com>
2023-12-04 07:18:09 +0100

    2017 day 15 part 2 (slow)

--- 2017/15/day.py
+++ 2017/15/day.py
@@ -3,29 +3,28 @@ import sys
 data = sys.stdin.read().splitlines()
 print(data)
 
-def gen(current, factor):
+def gen(current, factor, mod=1):
     while True:
         current = (current * factor) % 2147483647
-        yield current
+        if current % mod == 0:
+            yield current
 
 
 MASK = (1 << 16) - 1
- 
-count = 0
-for a, b, n in zip(
-    gen(int(data[0].split()[-1]), 16807),
-    gen(int(data[1].split()[-1]), 48271),
-    range(40_000_000),
-):
-    hit = (a & MASK) == (b & MASK)
-    count += hit
-    if n < 100 or n % 10000 == 0 or hit:
-        print(f'{a:15} {b:15} ({n}) {hit}')
+
+def solve(mods, amount):
+    count = 0
+    for a, b, n in zip(
+        gen(int(data[0].split()[-1]), 16807, mods.get('A', 1)),
+        gen(int(data[1].split()[-1]), 48271, mods.get('B', 1)),
+        range(amount),
+    ):
+        hit = (a & MASK) == (b & MASK)
+        count += hit
+        if n < 100 or n % 10000 == 0 or hit:
+            print(f'{a:15} {b:15} ({n}) {hit}')
+    return count
 
 
-print('*** part 1:', count)
-
-
-
-
-print('*** part 2:', ...)
+print('*** part 1:', solve({}, 40_000_000))
+print('*** part 2:', solve({'A': 4, 'B': 8}, 5_000_000))
