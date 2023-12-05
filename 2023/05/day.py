fcad90ce5fb3ac6feb7a0bb276df05bd20bf7682
Petr Viktorin <encukou@gmail.com>
2023-12-05 20:11:05 +0100

    2023 day 05 part 2

--- 2023/05/day.py
+++ 2023/05/day.py
@@ -1,5 +1,6 @@
 import sys
 from pprint import pprint
+import operator
 
 data = sys.stdin.read().splitlines()
 print(data)
@@ -20,93 +21,66 @@ for line in data[2:] + ['']:
         current_key, sep, next_key = line.split(' ')[0].split('-')
         current_map = []
     else:
-        current_start, next_start, length = [int(n) for n in line.split()]
+        next_start, current_start, length = [int(n) for n in line.split()]
         current_map.append((
-            range(current_start, current_start + length + 1),
-            range(next_start, next_start + length + 1),
+            range(current_start, current_start + length),
+            next_start - current_start,
         ))
 
 pprint(maps)
 
-result = []
-for seed in seeds:
-    current = seed
-    for current_map in maps:
-        last = current
-        for to_range, from_range in current_map:
-            if current in from_range:
-                current = current - from_range.start + to_range.start
-                print(f'{last}->{current} ({from_range}->{to_range})')
-                break
-        else:
-            print(f'{last}->{current} (default)')
-    print(f'location {current}')
-    result.append(current)
-
-
-print('*** part 1:', min(result))
-# 433542010 wrong
-
-def ranglen(start, length):
-    return range(start, start + length)
-
 pprint(maps)
 def map_ranges(current_ranges, current_map):
+    print(end='C '); pprint(current_map)
+    prev = list(current_ranges)
+    print('R', current_ranges)
+    current_ranges = merge_ranges(current_ranges)
+    print('M', current_ranges)
+    current_ranges = split_ranges(
+        current_ranges,
+        [r.start for r, d in current_map] + [r.stop for r, d in current_map]
+    )
     result = []
-    while current_ranges:
-        print(f'{result} || {current_ranges}')
-        c_range = current_ranges.pop()
-        for to_range, from_range in current_map:
-            shift = to_range.start - from_range.start
-            if c_range.stop <= from_range.start:
-                "no overlap"
-            elif c_range.start >= from_range.stop:
-                "no overlap"
-            elif from_range.start <= c_range.start and from_range.stop >= c_range.stop:
-                "inside"
-                dest = ranglen(c_range.start + shift, len(c_range))
-                print(f'! {c_range} is in {from_range}->{to_range} ({shift:+}): {dest}')
-                result.append(dest)
-                break
-            elif from_range.start <= c_range.start and from_range.stop < c_range.stop:
-                "start match"
-                overlap_len = from_range.stop - c_range.start
-                first, rest = c_range[:overlap_len], c_range[overlap_len:]
-                new = range(first.start + shift, first.stop + shift)
-                print(f'! {c_range} start in {from_range}->{to_range} ({shift:+}): {first}->{new}; {rest=}')
-                result.append(new)
-                current_ranges.append(rest)
-                break
-            elif from_range.start > c_range.start and from_range.stop >= c_range.stop:
-                "end match"
-                keep_len = from_range.start - c_range.start
-                rest, last = c_range[:keep_len], c_range[keep_len:]
-                new = range(last.start + shift, last.stop + shift)
-                print(f'! {c_range} end in {from_range}->{to_range} ({shift:+}): {rest=}; {last}->{new}')
-                result.append(new)
-                current_ranges.append(rest)
-                break
-            else:
-                "partitioning"
-                keep_len = c_range.start - from_range.start
-                overlap_len = len(c_range)
-                first, rest = c_range[:keep_len], c_range[keep_len:]
-                rest, last = rest[:overlap_len], rest[overlap_len:]
-                new = range(rest.start + shift, rest.stop + shift)
-                print(f'! {c_range} spans {from_range}->{to_range} ({shift:+}): {first=}; {rest}->{new}; {last=}')
-                result.append(new)
-                current_ranges.append(first)
-                current_ranges.append(last)
+    for r in current_ranges:
+        for mr, d in current_map:
+            if r.start in mr:
                 break
         else:
-            "no match"
-            print(f'! {c_range} stays {c_range}')
-            result.append(c_range)
-    print(f'{result} || {current_ranges}')
+            d = 0
+        new = range(r.start+d, r.stop+d)
+        print(f'{r} {d:+} -> {new}')
+        result.append(new)
+    assert sum(len(r) for r in prev) == sum(len(r) for r in result)
     return result
 
 def range_key(r):
     return r.start
+
+def split_ranges(ranges, points):
+    remaining = sorted(ranges, key=range_key)
+    verify_ranges(remaining)
+    remaining.reverse()
+    points = sorted(set(points), reverse=True)
+    result = []
+    while remaining:
+        print('S', result, remaining, points)
+        current = remaining.pop()
+        if not points:
+           result.append(current)
+        elif points[-1] <= current.start:
+            remaining.append(current)
+            points.pop()
+        elif points[-1] >= current.stop:
+            result.append(current)
+        elif points[-1] in current:
+            result.append(range(current.start, points[-1]))
+            remaining.append(range(points[-1], current.stop))
+        else:
+            raise ValueError()
+    print('s', result, remaining, points)
+    assert sum(len(r) for r in ranges) == sum(len(r) for r in result)
+    verify_ranges(result, op=operator.__le__)
+    return result
 
 def merge_ranges(ranges):
     ranges.sort(key=range_key)
@@ -119,21 +93,39 @@ def merge_ranges(ranges):
             result.append(current)
             current = next
     result.append(current)
+    verify_ranges(result)
     return result
 
+def verify_ranges(ranges, op=operator.__lt__):
+    for r in ranges:
+        assert r.start < r.stop
+    for a, b in zip(ranges, ranges[1:]):
+        print('V', a, b, op)
+        assert op(a.stop, b.start)
+
+def solve(current_ranges, maps):
+    print(f'{current_ranges=}')
+    for i, current_map in enumerate(maps):
+        print()
+        print(f'MAP {i}')
+        current_ranges = map_ranges(current_ranges, current_map)
+        current_ranges.sort(key=range_key)
+        print(f'{current_ranges}')
+        current_ranges = merge_ranges(current_ranges)
+        print(f'{current_ranges}')
+
+    print(f'{current_ranges}')
+    return current_ranges[0].start
+
+current_ranges = [range(s, s + 1) for s in seeds]
+
+print('*** part 1:', solve(current_ranges, maps))
+# 433542010 wrong
+
 print()
-current_ranges = [ranglen(s, l) for s, l in zip(seeds[0::2], seeds[1::2])]
-print(f'{current_ranges=}')
-for i, current_map in enumerate(maps):
-    print()
-    print(f'MAP {i}')
-    current_ranges = map_ranges(current_ranges, current_map)
-    current_ranges.sort(key=range_key)
-    print(f'{current_ranges}')
-    current_ranges = merge_ranges(current_ranges)
-    print(f'{current_ranges}')
+current_ranges = [range(s, s + l) for s, l in zip(seeds[0::2], seeds[1::2])]
 
-print(f'{current_ranges}')
-
-print('*** part 2:', current_ranges[0].start)
+print('*** part 2:', solve(current_ranges, maps))
 # 54305338 too high
+# 137201536 too high
+# 10834441 too high
