dfecfb8869b94130707b6654d05a5dac066b55c5
Petr Viktorin <encukou@gmail.com>
2023-12-08 06:18:43 +0100

    2023 day 08 part 2

--- 2023/08/day.py
+++ 2023/08/day.py
@@ -1,8 +1,9 @@
 import sys
 import re
 import itertools
+import math
 
-MAP_RE = re.compile(r'([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)')
+MAP_RE = re.compile(r'(\w+) = \((\w+), (\w+)\)')
 
 data = sys.stdin.read().splitlines()
 
@@ -14,13 +15,15 @@ def solve(data):
     for line in data[2:]:
         match = MAP_RE.match(line)
         desert_map[match[1]] = {'L': match[2], 'R': match[3]}
+    return solve_maze(instructions, desert_map, 'AAA', (lambda c: c == 'ZZZ'))
 
-    current = 'AAA'
+def solve_maze(instructions, desert_map, start, check_end):
+    current = start
     for turn_no, instruction in enumerate(itertools.cycle(instructions), start=1):
         entry = desert_map[current]
         print(turn_no, current, entry)
         current = entry[instruction]
-        if current == 'ZZZ':
+        if check_end(current):
             print(turn_no)
             return turn_no
 
@@ -33,7 +36,33 @@ ZZZ = (ZZZ, ZZZ)
 
 print('*** part 1:', solve(data))
 
+def part2(data):
+    print(data)
+    instructions = data[0]
+    assert not data[1]
+    desert_map = {}
+    for line in data[2:]:
+        match = MAP_RE.match(line)
+        desert_map[match[1]] = {'L': match[2], 'R': match[3]}
+    lengths = []
+    for start in (place for place in desert_map if place.endswith('A')):
+        num_steps = solve_maze(instructions, desert_map, start, (lambda c: c.endswith('Z')))
+        lengths.append(num_steps)
+    print(lengths)
+    return math.lcm(*lengths)
 
 
+assert part2("""LR
 
-print('*** part 2:', ...)
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+""".splitlines()) == 6
+
+
+print('*** part 2:', part2(data))
