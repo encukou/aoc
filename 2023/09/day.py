8cc9086457b07ed83db595588ea8932252c9df3b
Petr Viktorin <encukou@gmail.com>
2023-12-09 06:33:41 +0100

    2023 day 09 part 1

---
+++ 2023/09/day.py
@@ -0,0 +1,25 @@
+import sys
+
+data = sys.stdin.read().splitlines()
+print(data)
+
+total = 0
+for line in data:
+    nums = [int(n) for n in line.split()]
+    deltas = [nums]
+    while any(last_deltas := deltas[-1]):
+        print(last_deltas)
+        deltas.append([b-a for a, b in zip(last_deltas, last_deltas[1:])])
+        print('!', deltas)
+    last_deltas.append(0)
+    for s, d in zip(reversed(deltas), reversed(deltas[:-1])):
+        d.append(d[-1] + s[-1])
+        print('*', d)
+    total += deltas[0][-1]
+
+print('*** part 1:', total)
+
+
+
+
+print('*** part 2:', ...)
