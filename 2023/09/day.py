6b4ae32524009243897a7ece30df921a753a692a
Petr Viktorin <encukou@gmail.com>
2023-12-09 06:33:54 +0100

    2023 day 09 part 2

--- 2023/09/day.py
+++ 2023/09/day.py
@@ -19,7 +19,21 @@ for line in data:
 
 print('*** part 1:', total)
 
+total = 0
+for line in data:
+    nums = list(reversed([int(n) for n in line.split()]))
+    deltas = [nums]
+    while any(last_deltas := deltas[-1]):
+        print(last_deltas)
+        deltas.append([b-a for a, b in zip(last_deltas, last_deltas[1:])])
+        print('!', deltas)
+    last_deltas.append(0)
+    for s, d in zip(reversed(deltas), reversed(deltas[:-1])):
+        d.append(d[-1] + s[-1])
+        print('*', d)
+    total += deltas[0][-1]
 
 
 
-print('*** part 2:', ...)
+
+print('*** part 2:', total)
