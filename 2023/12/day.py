6dc5c8da5ab3124beb53183d503df6a3e97ac3f4
Petr Viktorin <encukou@gmail.com>
2023-12-12 07:09:31 +0100

    2023 day 12 part 2

--- 2023/12/day.py
+++ 2023/12/day.py
@@ -1,57 +1,49 @@
 import sys
 import itertools
 import re
+import functools
 
 data = sys.stdin.read().splitlines()
 print(data)
 
+@functools.lru_cache()
 def generate_spaces(initial, record, groups):
     remaining = sum(groups)
-    start = 1 if initial and groups else 0
+    start = 1 if (not initial) and (groups) else 0
     end = len(record) - remaining
-    try:
-        idx = record.index('#')
-    except ValueError:
-        pass
-    else:
+    if (idx := record.find('#')) != -1:
         end = min(end, idx)
-    print(f'{initial!r} \033[91m.\033[0m {record!r} {groups} {start}..{end}')
+    result = 0
     for i in range(start, end+1):
-        if '#' in record[:i]:
-            print('!!!!')
-            break
-        yield from generate_groups(initial + '.' * i, record[i:], groups)
+        assert '#' not in record[:i]
+        num = generate_groups(record[i:], groups)
+        result += num
+    #print(f'{record!r} {groups} {start}..{end} {result}')
+    return result
 
-def generate_groups(initial, record, groups):
-    print(f'{initial!r} \033[91m#\033[0m {record!r} {groups}')
+@functools.lru_cache()
+def generate_groups(record, groups):
     if not groups:
         if not record:
-            yield initial
-        return
+            return 1
+        return 0
     if '.' in record[:groups[0]]:
-        return
-    yield from generate_spaces(initial + '#' * groups[0], record[groups[0]:], groups[1:])
+        return 0
+    return generate_spaces(False, record[groups[0]:], groups[1:])
 
-total = 0
-for lineno, line in enumerate(data):
-    record, groups = line.split()
-    groups = [int(n) for n in groups.split(',')]
+def solve(data, repeats=1):
+    total = 0
+    for lineno, line in enumerate(data):
+        record, groups = line.split()
+        record = '?'.join([record] * repeats)
+        groups = tuple([int(n) for n in groups.split(',')] * repeats)
 
-    missing = len(record) - sum(groups)
-    space_num = len(groups) + 1
-    print(f'[{lineno}.]', record, groups, missing, space_num)
+        print(f'[{lineno}.]', record, groups)
+        num = generate_spaces(True, record, groups)
+        total += num
+        print(f'[{lineno}] {num} -> {total}')
+    return total
 
-    num = 0
-    for result in generate_spaces('', record, groups):
-        print(result)
-        num += 1
-    total += num
-    print(f'[{lineno}] {num} -> {total}')
+print('*** part 1:', solve(data))
 
-
-print('*** part 1:', total)
-# 7050 wrong
-
-
-
-print('*** part 2:', ...)
+print('*** part 2:', solve(data, 5))
