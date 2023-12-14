5ec74f193465d58aa9f0d6568efb58bb0036b420
Petr Viktorin <encukou@gmail.com>
2023-12-14 07:05:06 +0100

    2023 day 14: Some cleanup

--- 2023/14/day.py
+++ 2023/14/day.py
@@ -29,7 +29,8 @@ def get_load(platform):
     for r, row in enumerate(reversed(platform), start=1):
         new = (row == 'O').sum() * r
         total += new
-        print(r, row, new, total)
+        if len(row) < 20:
+            print(r, row, new, total)
     return total
 
 print('*** part 1:', get_load(tilt_west(platform.T).T))
@@ -42,45 +43,43 @@ platform = numpy.array([
 def do_cycle(platform):
     #print('North')
     platform = tilt_west(platform.T).T
-    #print(platform)
     #print('West')
     platform = tilt_west(platform)
-    #print(platform)
     #print('South')
     platform = numpy.fliplr(tilt_west(numpy.fliplr(platform.T))).T
-    #print(platform)
     #print('East')
     platform = numpy.fliplr(tilt_west(numpy.fliplr(platform)))
-    #print(platform)
     return platform
 
-def encode(platform):
-    return sum((bool(bit) << i) for i, bit in enumerate((platform == 'O').flatten()))
+def get_code(platform):
+    return sum(
+        (bool(bit) << i)
+        for i, bit in enumerate((platform == 'O').flatten())
+    )
 
+NUM_CYCLES = 1000000000
 print(platform)
 seen = {}
-for n in range(1000000000):
-    print(f'{n}/1000000000')
+for n in range(NUM_CYCLES):
+    print(f'{n}/{NUM_CYCLES}')
     platform = do_cycle(platform)
-    enc = encode(platform)
-    print(enc)
-    if n < 10:
-        for row in platform:
-            print(''.join(row))
-    if enc in seen:
+    print(platform)
+    platform_code = get_code(platform)
+    #print(f'{platform_code=}')
+    if platform_code in seen:
         break
-    seen[enc] = n
-cycle_length = n - seen[enc]
-print('L', cycle_length)
-for mult in 1000000000, 100000000, 10000000, 1000000, 100000, 1000, 1:
-    for i in range(1000):
-        if n + i*mult*cycle_length < 1000000000:
-            n += i*mult*cycle_length
-        else:
-            break
-        print(n)
-for n in range(n+1, 1000000000):
-    print(f'{n}/1000000000')
+    seen[platform_code] = n
+loop_length = n - seen[platform_code]
+print(f'{loop_length=}')
+remaining = NUM_CYCLES - n
+num_skipped_loops = remaining // loop_length
+print(f'{num_skipped_loops=}')
+num_skipped_cycles = num_skipped_loops * loop_length
+print(f'{num_skipped_cycles=}')
+n += num_skipped_cycles
+for n in range(n+1, NUM_CYCLES):
+    print(f'{n}/{NUM_CYCLES}')
     platform = do_cycle(platform)
+    print(platform)
 
 print('*** part 2:', get_load(platform))
