899a4870ff2b76f5b3f860de96c45b52487354ab
Petr Viktorin <encukou@gmail.com>
2023-12-15 06:23:36 +0100

    2023 day 15 part 2

--- 2023/15/day.py
+++ 2023/15/day.py
@@ -1,4 +1,5 @@
 import sys
+import pprint
 
 data = sys.stdin.read().strip()
 print(data)
@@ -15,14 +16,37 @@ assert compute_hash('HASH') == 52
 
 total = 0
 for word in data.split(','):
-    now = compute_hash(word)
-    total += now
-    print(word, now, total)
+    new = compute_hash(word)
+    total += new
+    print(word, new, total)
 
 
 print('*** part 1:', total)
 
+boxes = {n: {} for n in range(256)}
+for word in data.split(','):
+    if '=' in word:
+        label, length = word.split('=')
+        boxes[compute_hash(label)][label] = int(length)
+    elif word.endswith('-'):
+        label = word[:-1]
+        try:
+            del boxes[compute_hash(label)][label]
+        except KeyError:
+            pass
+    else:
+        raise ValueError
+    print(word)
+    for box_num, box in boxes.items():
+        if box:
+            print('Box', box_num, box)
+
+total = 0
+for box_num, box in boxes.items():
+    for lens_num, length in enumerate(box.values()):
+        new = (1 + box_num) * (1 + lens_num) * length
+        total += new
+        print(box_num, lens_num, length, ':', new, '->', total)
 
 
-
-print('*** part 2:', ...)
+print('*** part 2:', total)
