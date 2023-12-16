03a2ef7731698f9537fa3f1b05cd6c98506d2fab
Petr Viktorin <encukou@gmail.com>
2023-12-16 22:21:50 +0100

    2017 day 16 part 2

--- 2017/16/day.py
+++ 2017/16/day.py
@@ -3,7 +3,7 @@ import sys
 data = sys.stdin.read().strip()
 print(data)
 
-def dance(start_pos, instructions):
+def dance(start_pos, instructions, verbose=True):
     positions = list(start_pos)
     for instruction in instructions.split(','):
         if instruction.startswith('s'):
@@ -22,17 +22,37 @@ def dance(start_pos, instructions):
             positions[a], positions[b] = positions[b], positions[a]
         else:
             raise ValueError(instruction)
-        print(instruction, ''.join(positions))
+        if verbose:
+            print(instruction, ''.join(positions))
     return ''.join(positions)
 
 assert dance('abcde', 's1,x3/4,pe/b') == 'baedc'
 
 
-start_pos = 'abcdefghijklmnop'
-assert len(start_pos) == 16
+if len(data) < 50:
+    start_pos = 'abcde'
+else:
+    start_pos = 'abcdefghijklmnop'
+    assert len(start_pos) == 16
 print('*** part 1:', dance(start_pos, data))
 
+NUM_DANCES = 1000000000
+positions = start_pos
+seen = {}
+for n in range(NUM_DANCES):
+    print(n, positions)
+    if positions in seen:
+        break
+    seen[positions] = n
+    positions = dance(positions, data, verbose=False)
+loop_size = n - seen[positions]
+remaining = NUM_DANCES - n
+loops = remaining // loop_size
+n += loop_size * loops
+print(f'{loop_size=} {remaining=} {loops=} {n=}')
+for n in range(n, NUM_DANCES):
+    print(n, positions)
+    positions = dance(positions, data, verbose=False)
 
 
-
-print('*** part 2:', ...)
+print('*** part 2:', positions)
