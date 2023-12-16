2916f4cf6b5662919caf719dff3d8b44b0b3f7cc
Petr Viktorin <encukou@gmail.com>
2023-12-16 22:34:51 +0100

    2017 day 17 part 2

--- 2017/17/day.py
+++ 2017/17/day.py
@@ -17,7 +17,18 @@ for i in range(1, 2017+1):
 
 print('*** part 1:', buf[(pos+1) % len(buf)])
 
+bufsize = 1
+pos = 0
+after_zero = None
+for i in range(1, 50000000+1):
+    pos += step
+    pos %= bufsize
+    pos += 1
+    bufsize += 1
+    if pos == 1:
+        after_zero = i
+    if i % 50007 == 0:
+        print(f'{i=} {pos=} +{step} {bufsize=}')
 
-
-
-print('*** part 2:', ...)
+print('*** part 2:', after_zero)
+# 1222153 too low
