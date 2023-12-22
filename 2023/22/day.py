8c392dfc83ee85e8c42d3bee2b4a26a0f27970af
Petr Viktorin <encukou@gmail.com>
2023-12-22 13:02:19 +0100

    2023 day 22 part 2

--- 2023/22/day.py
+++ 2023/22/day.py
@@ -2,19 +2,18 @@ import sys
 from dataclasses import dataclass
 from pprint import pprint
 from collections import defaultdict
+from functools import cached_property
 
 data = sys.stdin.read().splitlines()
 print(data)
 
-@dataclass
 class Brick:
     ranges: list
-    resting: bool = False
+    resting: bool
 
-    @classmethod
-    def parse(cls, line):
+    def __init__(self, line):
         starts, ends = line.split('~')
-        ranges = [
+        self.ranges = [
             range(s, e)
             for s, e
             in zip(
@@ -22,7 +21,7 @@ class Brick:
                 (int(n)+1 for n in ends.split(',')),
             )
         ]
-        return cls(ranges, ranges[-1].start == 1)
+        self.resting = (self.ranges[-1].start == 1)
 
     def __repr__(self):
         return ''.join([
@@ -35,23 +34,32 @@ class Brick:
     def sort_key(self):
         return self.ranges[-1].start, self.ranges[-1].stop
 
+    @cached_property
+    def supporters(self):
+        return set()
+
+    @cached_property
+    def supportees(self):
+        return set()
+
 bricks = []
 for line in data:
-    bricks.append(Brick.parse(line))
+    bricks.append(Brick(line))
 pprint(bricks)
 
 bricks.sort(key=Brick.sort_key)
 while not all(b.resting for b in bricks):
     print('falling')
-    for bi, brick in enumerate(bricks):
+    for brick in bricks:
         if brick.resting:
             continue
         rx, ry, rz = brick.ranges
         zs_below = [1]
         resting = True
         fall = True
-        for obi, obrick in enumerate(bricks):
-            if bi == obi:
+        supported_by = set()
+        for obrick in bricks:
+            if brick == obrick:
                 continue
             ox, oy, oz = obrick.ranges
             if rz.start < oz.stop:
@@ -68,18 +76,16 @@ while not all(b.resting for b in bricks):
             continue
         fall_z = max(zs_below)
         assert fall_z <= rz.start
-        #print('fall')
+        print('fall')
         brick.ranges[-1] = range(fall_z, fall_z + len(rz))
         brick.resting = True
-        #print(brick, max(zs_below))
+        print(brick, max(zs_below))
     pprint(bricks)
 
-supports = defaultdict(list)
-for bi, brick in enumerate(bricks):
+for brick in bricks:
     rx, ry, rz = brick.ranges
-    supported_by = []
-    for obi, obrick in enumerate(bricks):
-        if bi == obi:
+    for obrick in bricks:
+        if brick == obrick:
             continue
         ox, oy, oz = obrick.ranges
         found = False
@@ -89,17 +95,45 @@ for bi, brick in enumerate(bricks):
             and ry.stop > oy.start
             and oz.start == rz.stop
         ):
-            supported_by.append(obi)
-            supports[obi].append(bi)
-    print(brick, supported_by)
-pprint(supports)
+            obrick.supporters.add(brick)
+            brick.supportees.add(obrick)
 do_not_break = set()
-for bi, supported_by in supports.items():
-    if len(supported_by) == 1:
-        do_not_break.add(supported_by[0])
+for brick in bricks:
+    if len(brick.supporters) == 1:
+        [lone_supporter] = brick.supporters
+        do_not_break.add(lone_supporter)
 print(do_not_break)
 
 print('*** part 1:', len(bricks) - len(do_not_break))
 
+bricks.sort(key=Brick.sort_key)
+for brick in bricks:
+    removed = {brick}
+    to_check = list(brick.supportees)
+    seen = set()
+    print()
+    print(f'Check for {brick}')
+    while to_check:
+        to_check.sort(key=Brick.sort_key)
+        current = to_check.pop(0)
+        if current in seen:
+            print(f'{current} checked!')
+            continue
+        seen.add(current)
+        remaining_supporters = current.supporters - removed
+        if remaining_supporters:
+            print(f'{current} would still be supported by {remaining_supporters}')
+        else:
+            print(
+                f'{current} would fall! '
+                + f'It supports {len(current.supportees)} more.',
+            )
+            removed.add(current)
+            to_check.extend(current.supportees)
+    brick.score = len(removed) - 1
+    print(f'{brick.score=}')
 
-print('*** part 2:', ...)
+print('*** part 2:', sum(b.score for b in bricks))
+# Wrong: 1470
+# 31348 too low!
+# 67468
