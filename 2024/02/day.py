cb5a04fe37f1763db7b5521eaa2589aa57e57637
Petr Viktorin <encukou@gmail.com>
2024-12-02 06:11:40 +0100

    2024 day 02 part 2

--- 2024/02/day.py
+++ 2024/02/day.py
@@ -9,16 +9,30 @@ def is_safe_increasing(numbers):
             return False
     return True
 
-num_safe = 0
-for line in data:
-    numbers = [int(n) for n in line.split()]
-    if is_safe_increasing(numbers) or is_safe_increasing(numbers[::-1]):
-        num_safe += 1
+def evaluate_report_undampened(numbers):
+    return is_safe_increasing(numbers) or is_safe_increasing(numbers[::-1])
+
+def evaluate_data(data, evaluate_report):
+    num_safe = 0
+    for line in data:
+        numbers = [int(n) for n in line.split()]
+        if evaluate_report(numbers):
+            print('safe', numbers)
+            num_safe += 1
+        else:
+            print('NOT!', numbers)
+    return num_safe
+
+print('*** part 1:', evaluate_data(data, evaluate_report_undampened))
+
+def evaluate_report_dampened(numbers):
+    for i in range(len(numbers)):
+        dampened_numbers = numbers[:i] + numbers[i+1:]
+        if is_safe_increasing(dampened_numbers):
+            return True
+        if is_safe_increasing(dampened_numbers[::-1]):
+            return True
+    return False
 
 
-print('*** part 1:', num_safe)
-
-
-
-
-print('*** part 2:', ...)
+print('*** part 2:', evaluate_data(data, evaluate_report_dampened))
