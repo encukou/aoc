5a3a7066b288908fb8a9344e29991066efcbba17
Petr Viktorin <encukou@gmail.com>
2024-12-05 06:16:49 +0100

    2024 day 06 part 2

--- 2024/05/day.py
+++ 2024/05/day.py
@@ -1,3 +1,4 @@
+import collections
 import sys
 
 data = sys.stdin.read().splitlines()
@@ -17,6 +18,7 @@ for line in lines:
 print(updates)
 
 total = 0
+incorrect_updates = []
 for update in updates:
     print(update)
     ok = True
@@ -38,11 +40,30 @@ for update in updates:
         new = update[len(update) // 2]
         total += new
         print(new, total)
+    else:
+        incorrect_updates.append(update)
 
 
 print('*** part 1:', total)
 
+earlier = collections.defaultdict(set)
+later = collections.defaultdict(set)
+for a, b in rules:
+    earlier[b].add(a)
+    later[a].add(b)
 
+total = 0
+for update in incorrect_updates:
+    ordered = []
+    print(update)
+    for n in update:
+        for i in range(0, len(ordered)+1):
+            if set(ordered[:i]) <= earlier[n] and set(ordered[i:]) <= later[n]:
+                ordered.insert(i, n)
+                break
+        print(ordered)
+    new = ordered[len(ordered) // 2]
+    total += new
+    print(new, total)
 
-
-print('*** part 2:', ...)
+print('*** part 2:', total)
