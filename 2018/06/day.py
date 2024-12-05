1168ea0d2dbb8d1699ee762ad23b05d3c629c0e1
Petr Viktorin <encukou@gmail.com>
2024-12-05 06:41:11 +0100

    2018 day 06 part 2

--- 2018/06/day.py
+++ 2018/06/day.py
@@ -17,14 +17,17 @@ xrange = range(min_x-1, max_x+2)
 yrange = range(min_y-1, max_y+2)
 
 closests = {}
+distance_map = {}
 for x in xrange:
+    print(x, '/', max_x, len(closests))
     for y in yrange:
         distances = sorted(
             (abs(x - xx) + abs(y - yy), i) for i, (xx, yy) in enumerate(coords)
         )
+        distance_map[x, y] = sum(d for d, i in distances)
         if distances[0][0] != distances[1][0]:
             closests[x, y] = distances[0][1]
-        print(x, y, closests.get((x, y)), distances)
+        #print(x, y, closests.get((x, y)), distances)
 candidates = set(closests.values())
 candidates -= {closests.get((x, min_y)) for x in xrange}
 candidates -= {closests.get((x, max_y)) for x in xrange}
@@ -36,7 +39,26 @@ print(counter)
 
 print('*** part 1:', counter.most_common(1)[0][-1])
 
+if len(data) < 10:
+    target = 32
+else:
+    target = 10000
 
+for x in xrange:
+    for y in yrange:
+        print(distance_map[x, y], end=';')
+    print()
+total = sum(1 for d in distance_map.values() if d < target)
 
+print(total)
+def border_check(d):
+    assert d >= target, d
+for x in xrange:
+    border_check(distance_map[x, min_y])
+    border_check(distance_map[x, max_y])
+for y in yrange:
+    border_check(distance_map[min_x, y])
+    border_check(distance_map[max_x, y])
+print(total)
 
-print('*** part 2:', ...)
+print('*** part 2:', total)
