63171362e8709030cd469c916ee3647b6a8b428e
Petr Viktorin <encukou@gmail.com>
2024-12-06 22:28:34 +0100

    2018 day 08 part 2

--- 2018/08/day.py
+++ 2018/08/day.py
@@ -16,6 +16,23 @@ total = sum(gen_metadata(iter(data)))
 print('*** part 1:', total)
 
 
+def gen_value(it):
+    n_children = next(it)
+    n_metadata = next(it)
+    values = [gen_value(it) for i in range(n_children)]
+    metadata = [next(it) for i in range(n_metadata)]
+    if n_children:
+        value = 0
+        for index in metadata:
+            try:
+                value += values[index - 1]
+            except IndexError:
+                pass
+    else:
+        value = sum(metadata)
+    print(n_children, n_metadata, values, metadata, value)
+    return value
 
+total = gen_value(iter(data))
 
-print('*** part 2:', ...)
+print('*** part 2:', total)
