ec0c2afc2b096d5b48a895e9ce7ffd9367802024
Petr Viktorin <encukou@gmail.com>
2024-12-08 06:51:02 +0100

    2018 day 09, linked list

--- 2018/09/day.py
+++ 2018/09/day.py
@@ -1,8 +1,44 @@
 import re
 import sys
+import dataclasses
+import gc
+gc.disable()
 
 data = sys.stdin.read().splitlines()
 print(data)
+
+@dataclasses.dataclass
+class Marble:
+    value: int
+    next: 'Marble'
+    prev: 'Marble'
+
+    @classmethod
+    def new(cls, value):
+        self = cls(value, None, None)
+        self.next = self
+        self.prev = self
+        return self
+
+    def __iter__(self):
+        yield self
+        current = self.next
+        while current != self:
+            yield current
+            current = current.next
+
+    def __repr__(self):
+        return f'<{self.value}>'
+
+    def insert_after(self, value):
+        inserted = Marble(value, self.next, self)
+        self.next.prev = inserted
+        self.next = inserted
+
+    def pop(self):
+        self.next.prev = self.prev
+        self.prev.next = self.next
+        return self.value
 
 problems = []
 for line in data:
@@ -17,26 +53,27 @@ if len(problems) == 1:
 
 results = []
 for n_players, end in problems:
-    circle = [0]
-    current = 0
+    current = Marble.new(0)
     scores = [0 for i in range(n_players)]
-    for current_marble in range(1, end):
-        elf = current_marble % n_players
-        if current_marble < 25:
-            print(elf, current, circle)
-        elif current_marble % 1000 == 0:
-            print(f'{current_marble}/{end}', elf, current, len(circle), flush=True)
-        if current_marble % 23 == 0:
-            scores[elf] += current_marble
-            current -= 7
-            current %= len(circle)
-            scores[elf] += circle.pop(current)
+    for current_score in range(1, end):
+        elf = current_score % n_players
+        if current_score < 25:
+            print(elf, current, list(current), flush=True)
+        elif current_score % 1000 == 0:
+            print(f'{current_score}/{end}', elf, current, flush=True)
+        if current_score % 23 == 0:
+            scores[elf] += current_score
+            for i in range(6):
+                current = current.prev
+            popped = current
+            current = popped.prev
+            scores[elf] += popped.pop()
         else:
-            current += 2
-            current %= len(circle)
-            circle.insert(current, current_marble)
+            current = current.next.next
+            current.insert_after(current_score)
     results.append(max(scores))
+    gc.collect()
 
 
-print('*** part 1:', ' '.join(str(n) for n in results))
+print('*** part 0:', ' '.join(str(n) for n in results))
 
