fcf321abc9aa1608dbf51dc7a353a2b0677570f1
Petr Viktorin <encukou@gmail.com>
2024-12-08 06:25:09 +0100

    2018 day 09 part 2 (very slow)

--- 2018/09/day.py
+++ 2018/09/day.py
@@ -4,11 +4,19 @@ import sys
 data = sys.stdin.read().splitlines()
 print(data)
 
-results = []
+problems = []
 for line in data:
     match = re.match(r'(\d+) players; last marble is worth (\d+) points', line)
     n_players = int(match[1])
     end = int(match[2]) + 1
+    problems.append((n_players, end))
+
+if len(problems) == 1:
+    n_players, end = problems[0]
+    problems.append((n_players, end * 100))
+
+results = []
+for n_players, end in problems:
     circle = [0]
     current = 0
     scores = [0 for i in range(n_players)]
@@ -16,6 +24,8 @@ for line in data:
         elf = current_marble % n_players
         if current_marble < 25:
             print(elf, current, circle)
+        elif current_marble % 1000 == 0:
+            print(f'{current_marble}/{end}', elf, current, len(circle), flush=True)
         if current_marble % 23 == 0:
             scores[elf] += current_marble
             current -= 7
@@ -30,7 +40,3 @@ for line in data:
 
 print('*** part 1:', ' '.join(str(n) for n in results))
 
-
-
-
-print('*** part 2:', ...)
