d177d48780a86ffc110561ac3c194bf950b9ecc5
Petr Viktorin <encukou@gmail.com>
2024-12-14 08:15:16 +0100

    2018 day 13 part 2

--- 2018/13/day.py
+++ 2018/13/day.py
@@ -1,7 +1,16 @@
+import dataclasses
 import sys
 
-data = sys.stdin.read().splitlines()
-print(data)
+data = sys.stdin.read()
+try:
+    data1, data2 = data.split('***')
+except ValueError:
+    data1 = data2 = data.splitlines()
+else:
+    data1 = data1.strip().splitlines()
+    data2 = data2.strip().splitlines()
+print(data1)
+print(data2)
 
 DIR_CHARS = {
     (-1, 0): '▲',
@@ -10,119 +19,139 @@ DIR_CHARS = {
     (1, 0): '▼',
 }
 
-tracks = {}
-cars = []
-for r, line in enumerate(data):
-    for c, char in enumerate(line):
-        match char:
-            case '\\':
-                if tracks.get((r-1, c), ' ') in '│┼╭╮':
-                    tracks[r, c] = '╰'
-                else:
-                    tracks[r, c] = '╮'
-            case '/':
-                if tracks.get((r-1, c), ' ') in '│┼╭╮':
-                    tracks[r, c] = '╯'
-                else:
-                    tracks[r, c] = '╭'
-            case '-':
-                tracks[r, c] = '─'
-            case '>':
-                tracks[r, c] = '─'
-                cars.append((r, c, 0, 1, 0))
-            case '<':
-                tracks[r, c] = '─'
-                cars.append((r, c, 0, -1, 0))
-            case '|':
-                tracks[r, c] = '│'
-            case 'v':
-                tracks[r, c] = '│'
-                cars.append((r, c, 1, 0, 0))
-            case '^':
-                tracks[r, c] = '│'
-                cars.append((r, c, -1, 0, 0))
-            case '+':
-                tracks[r, c] = '┼'
-            case ' ':
-                pass
-            case _:
-                raise ValueError(char)
+@dataclasses.dataclass
+class Cart:
+    r: int
+    c: int
+    dr: int
+    dc: int
+    mem: int
+    dead = False
+
+    def pos(self):
+        return self.r, self.c
+
+def parse_data(data):
+    tracks = {}
+    cars = []
+    for r, line in enumerate(data):
+        for c, char in enumerate(line):
+            match char:
+                case '\\':
+                    if tracks.get((r-1, c), ' ') in '│┼╭╮':
+                        tracks[r, c] = '╰'
+                    else:
+                        tracks[r, c] = '╮'
+                case '/':
+                    if tracks.get((r-1, c), ' ') in '│┼╭╮':
+                        tracks[r, c] = '╯'
+                    else:
+                        tracks[r, c] = '╭'
+                case '-':
+                    tracks[r, c] = '─'
+                case '>':
+                    tracks[r, c] = '─'
+                    cars.append(Cart(r, c, 0, 1, 0))
+                case '<':
+                    tracks[r, c] = '─'
+                    cars.append(Cart(r, c, 0, -1, 0))
+                case '|':
+                    tracks[r, c] = '│'
+                case 'v':
+                    tracks[r, c] = '│'
+                    cars.append(Cart(r, c, 1, 0, 0))
+                case '^':
+                    tracks[r, c] = '│'
+                    cars.append(Cart(r, c, -1, 0, 0))
+                case '+':
+                    tracks[r, c] = '┼'
+                case ' ':
+                    pass
+                case _:
+                    raise ValueError(char)
+    return tracks, cars
 
 def draw_map(tracks, cars):
-    cars = {(r, c): (dr, dc, m) for r, c, dr, dc, m in cars}
-    for r, row in enumerate(data):
-        for c, char in enumerate(row):
+    cars = {(car.r, car.c): car for car in cars}
+    for r, row in enumerate(data2):
+        for c, char in enumerate(data1[0]):
             if car := cars.get((r, c)):
-                dr, dc, m = car
-                print(end=DIR_CHARS[dr, dc])
+                print(end=DIR_CHARS[car.dr, car.dc])
             else:
                 print(end=tracks.get((r, c), ' '))
         print(flush=True)
 
-draw_map(tracks, cars)
-
 def gen_crashes(tracks, cars):
-    def add(r, c, dr, dc, m):
-        if (r, c) in new_car_positions:
-            if (r, c) not in collisions:
-                collisions.append((r, c))
-            new_cars[:] = [
-                (pr, pc, dr, dc, m) for pr, pc, dr, dc, m in new_cars
-                if (pr, pc) != (r, c)
-            ]
-        else:
-            new_car_positions.add((r, c))
-            new_cars.append((r, c, dr, dc, m))
-    def turn_left():
-        add(r-dc, c+dr, -dc, dr, m)
-    def go_straight():
-        add(r+dr, c+dc, dr, dc, m)
-    def turn_right():
-        add(r+dc, c-dr, dc, -dr, m)
+    def go_straight(car):
+        car.r += car.dr
+        car.c += car.dc
+        assert (car.dr*car.dc==0 and abs(car.dr+car.dc)==1), car
+        for other in cars:
+            if other is not car and other.pos() == car.pos() and not other.dead:
+                car.dead = True
+                other.dead = True
+                collisions.append((car.r, car.c))
+    def turn_left(car):
+        car.dr, car.dc = -car.dc, car.dr
+        go_straight(car)
+    def turn_right(car):
+        car.dr, car.dc = car.dc, -car.dr
+        go_straight(car)
     turn_number = 0
+    draw_map(tracks, cars)
     while True:
-        turn_number += 1
-        new_cars = []
-        new_car_positions = set()
         collisions = []
-        for r, c, dr, dc, m in cars:
-            match tracks[r, c], dr, dc, m:
-                case '─', 0, _, _: go_straight()
-                case '│', _, 0, _: go_straight()
+        cars.sort(key=Cart.pos)
+        for car in cars:
+            if car.dead:
+                continue
+            match tracks[car.r, car.c], car.dr, car.dc, car.mem:
+                case '─', 0, _, _: go_straight(car)
+                case '│', _, 0, _: go_straight(car)
                 case '┼', _, _, 0:
-                    m = 1
-                    turn_left()
+                    car.mem = 1
+                    turn_left(car)
                 case '┼', _, _, 1:
-                    m = 2
-                    go_straight()
+                    car.mem = 2
+                    go_straight(car)
                 case '┼', _, _, 2:
-                    m = 0
-                    turn_right()
-                case '╮', 0, _, _: turn_right()
-                case '╯', 0, _, _: turn_left()
-                case '╭', 0, _, _: turn_left()
-                case '╰', 0, _, _: turn_right()
-                case '╮', _, 0, _: turn_left()
-                case '╯', _, 0, _: turn_right()
-                case '╭', _, 0, _: turn_right()
-                case '╰', _, 0, _: turn_left()
+                    car.mem = 0
+                    turn_right(car)
+                case '╮', 0, _, _: turn_right(car)
+                case '╯', 0, _, _: turn_left(car)
+                case '╭', 0, _, _: turn_left(car)
+                case '╰', 0, _, _: turn_right(car)
+                case '╮', _, 0, _: turn_left(car)
+                case '╯', _, 0, _: turn_right(car)
+                case '╭', _, 0, _: turn_right(car)
+                case '╰', _, 0, _: turn_left(car)
                 case _:
-                    raise ValueError((tracks[r, c], r, c, dr, dc, m))
-        yield from collisions
-        cars = new_cars
+                    raise ValueError((tracks[car.r, car.c], car))
         if (
-            turn_number < 10
+            turn_number < 20
             or turn_number < 100 and turn_number % 10 == 0
             or turn_number < 1000 and turn_number % 100 == 0
         ):
             print(turn_number)
             draw_map(tracks, cars)
+        cars = [c for c in cars if not c.dead]
+        yield collisions, cars
+        turn_number += 1
 
-for r, c in gen_crashes(tracks, cars.copy()):
-    break
+for collisions, cars in gen_crashes(*parse_data(data1)):
+    if collisions:
+        for r, c in collisions:
+            break
+        break
 print(f'*** part 1: {c},{r}')
 
+for i, (collisions, cars) in enumerate(gen_crashes(*parse_data(data2))):
+    if collisions:
+        for r, c in collisions:
+            print(f'{i}: Boom! @ {r},{c}')
+        print(f'... {len(cars)} left')
+    if len(cars) == 1:
+        r, c = cars[0].pos()
+        break
 
-
-
-print('*** part 2:', ...)
+print(f'*** part 2: {c},{r}')
