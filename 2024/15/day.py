6f8a02e760af63237d4d9bb67879cab0b821d675
Petr Viktorin <encukou@gmail.com>
2024-12-15 06:52:18 +0100

    2024 day 15 part 2

--- 2024/15/day.py
+++ 2024/15/day.py
@@ -25,6 +25,9 @@ SMALL_EXAMPLE = """
 <^^>>>vv<v>>v<<
 """.strip().splitlines()
 
+class Blocked(Exception):
+    """Robot is blocked"""
+
 @dataclasses.dataclass
 class State:
     walls: dict
@@ -42,24 +45,27 @@ class State:
                     print(end='@')
                 elif (r, c) in self.walls:
                     print(end='#')
-                elif (r, c) in self.boxes:
-                    print(end='O')
+                elif b := self.boxes.get((r, c)):
+                    print(end=b)
                 else:
                     print(end='.')
             print()
         if moves:
             print([DIR_CHARS[m] for m in self.r_moves])
 
-    def move(self):
+    def move(self, part=1):
         move = self.r_moves.pop()
         left = len(self.r_moves)
-        self.do_move(move)
+        if part == 1:
+            self.simple_move(move)
+        else:
+            self.complex_move(move)
         if left < 100 or left % 100 == 0:
             print(f"Move {DIR_CHARS[move]}:")
             self.dump(moves=False)
             print()
 
-    def do_move(self, move):
+    def simple_move(self, move):
         move_r, move_c = move
         new_r = self.r + move_r
         new_c = self.c + move_c
@@ -78,13 +84,55 @@ class State:
         self.r = new_r
         self.c = new_c
 
+    def complex_move(self, move):
+        move_r, move_c = move
+        new_r = self.r + move_r
+        new_c = self.c + move_c
+        positions_to_check = []
+        boxes_to_move = {}
+        def add_box(r, c):
+            if (r, c) in self.walls:
+                raise Blocked()
+            if (r, c) in boxes_to_move:
+                return
+            box = self.boxes.get((r, c))
+            if box == '[':
+                other_c = c + 1
+                other_box = ']'
+            elif box == ']':
+                other_c = c - 1
+                other_box = '['
+            else:
+                return
+            assert other_box == self.boxes[r, other_c]
+            boxes_to_move[r, c] = box
+            boxes_to_move[r, other_c] = other_box
+            positions_to_check.append((r+move_r, c+move_c))
+            positions_to_check.append((r+move_r, other_c+move_c))
+        try:
+            add_box(new_r, new_c)
+            while positions_to_check:
+                r, c = positions_to_check.pop()
+                add_box(r, c)
+        except Blocked:
+            #print('Blocked!')
+            return
+        #print(boxes_to_move)
+        for r, c in boxes_to_move:
+            self.boxes.pop((r, c))
+        for (r, c), box in boxes_to_move.items():
+            self.boxes[r+move_r, c+move_c] = box
+        self.r = new_r
+        self.c = new_c
+
     def get_score(self):
         total = 0
-        for r, c in self.boxes:
-            total += 100 * r + c
+        for (r, c), box in self.boxes.items():
+            if box in 'O[':
+                total += 100 * r + c
         return total
 
-def load(data):
+def load(data, part=1):
     it = iter(data)
     walls = {}
     boxes = {}
@@ -92,27 +140,41 @@ def load(data):
         if not line:
             break
         for c, char in enumerate(line):
-            match char:
-                case '#':
-                    walls[r, c] = '#'
-                case 'O':
-                    boxes[r, c] = 'O'
-                case '@':
-                    robot_pos = r, c
-                case '_':
-                    raise ValueError(char)
+            if part == 1:
+                match char:
+                    case '#':
+                        walls[r, c] = '#'
+                    case 'O':
+                        boxes[r, c] = 'O'
+                    case '@':
+                        robot_pos = r, c
+                    case '_':
+                        raise ValueError(char)
+            else:
+                c *= 2
+                match char:
+                    case '#':
+                        walls[r, c] = '#'
+                        walls[r, c+1] = '#'
+                    case 'O':
+                        boxes[r, c] = '['
+                        boxes[r, c+1] = ']'
+                    case '@':
+                        robot_pos = r, c
+                    case '_':
+                        raise ValueError(char)
     moves = []
     for line in it:
         moves.extend(DIRS[char] for char in line)
     moves.reverse()
-    state = State(walls, boxes, *robot_pos, moves, r, len(data[0]))
+    state = State(walls, boxes, *robot_pos, moves, r, len(data[0])*part)
     state.dump()
     return state
 
-def solve(data):
-    state = load(data)
+def solve(data, part=1):
+    state = load(data, part)
     while state.r_moves:
-        state.move()
+        state.move(part)
         print(len(state.r_moves), 'left', flush=True)
     return state.get_score()
 
@@ -122,7 +184,20 @@ if len(data) < 50:
 
 print('*** part 1:', solve(data))
 
+SMALL_EXAMPLE = """
+#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^
+""".strip().splitlines()
+
+if len(data) < 50:
+    solve(SMALL_EXAMPLE, part=2)
 
 
-
-print('*** part 2:', ...)
+print('*** part 2:', solve(data, part=2))
