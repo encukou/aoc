4ab82b9d55f25d1301e4f123fe01e330906355f8
Petr Viktorin <encukou@gmail.com>
2024-12-17 10:11:55 +0100

    2024 day 17 part 2 (generic)

--- 2024/17/day.py
+++ 2024/17/day.py
@@ -95,14 +95,18 @@ assert example_machine.A == 0
 assert run(Machine([1,7], B=29)).B == 26
 assert run(Machine([4, 0], B=2024, C=43690)).B == 44354
 
-registers = {}
-for line in data:
-    if match := re.fullmatch(r'Register (.): (\d+)', line):
-        registers[Register[match[1]]] = int(match[2])
-    elif match := re.fullmatch(r'Program: (.+)', line):
-        program = [int(n) for n in match[1].split(',')]
-    elif line.strip():
-        raise ValueError(line)
+def load_data(data):
+    registers = {}
+    for line in data:
+        if match := re.fullmatch(r'Register (.): (\d+)', line):
+            registers[Register[match[1]]] = int(match[2])
+        elif match := re.fullmatch(r'Program: (.+)', line):
+            program = [int(n) for n in match[1].split(',')]
+        elif line.strip():
+            raise ValueError(line)
+    return registers, program
+
+registers, program = load_data(data)
 print(registers)
 print(program)
 
@@ -115,43 +119,15 @@ result = ','.join(str(n) for n in machine.output)
 print('*** part 1:', result)
 
 if data[0] == 'Register A: 729':
+    # Part 2 doesn't use example data
     exit()
 
-# Specific to my input...
-def run_py(A):
-    B = C = 0
-    result = []
-    while True:
-        B = (A % 8) ^ 5
-
-        C = A // (2 ** B)
-        A = A // 8
-        B = B ^ C ^ 6
-
-        result.append(B % 8)
-        if not A:
-            break
-    return result
-
-new_result = run_py(47719761)
-print('new:', new_result)
-assert new_result == machine.output, (new_result, machine.output)
-
-for i in range(1000):
-    print(i, oct(i), run_py(i))
-
-i = 1 << (3 * (len(program) - 1))
-assert len(run_py(i-1)) < len(program)
-assert len(run_py(i)) == len(program)
-for n in range(i, i+10):
-    result = run_py(n)
-    print(n, oct(n), len(result), result)
-
 print()
-increment = i
-n = increment
+n = 1
+while len(run(Machine(program, A=n)).output) < len(program):
+    n *= 8
 while True:
-    result = run_py(n)
+    result = run(Machine(program, A=n)).output
     assert len(result) == len(program)
     for i, (a, b) in enumerate(reversed(list(zip(result, program)))):
         if a != b:
@@ -159,7 +135,7 @@ while True:
     else:
         break
     print(n, oct(n), len(result), result[:i], result[i:], n)
-    increment = 1 << ((len(program)-i-1) * 3)
+    increment = 8 ** (len(program)-i-1)
     print(i, a, b, oct(increment))
     n += increment
 
