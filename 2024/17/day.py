6fcf7156272ef47c0fbd72074d8cdcd6cd90fa9c
Petr Viktorin <encukou@gmail.com>
2024-12-17 10:04:18 +0100

    2024 day 17 part 2 (specific to my input)

--- 2024/17/day.py
+++ 2024/17/day.py
@@ -11,6 +11,9 @@ class Register(enum.Enum):
     B = 5
     C = 6
 
+    def __repr__(self):
+        return f'<{self.name}>'
+
 @dataclasses.dataclass
 class Machine:
     program: list
@@ -73,6 +76,7 @@ def run(machine):
                 machine.B = machine.B ^ machine.C
             case 5:  # out
                 machine.output.append(combo_op() % 8)
+                print('>', machine)
             case 0:  # bdv
                 machine.B = machine.A // (2 ** combo_op())
             case 7:  # cdv
@@ -102,13 +106,63 @@ for line in data:
 print(registers)
 print(program)
 
+print()
+print()
 machine = run(Machine(program, registers))
 
 result = ','.join(str(n) for n in machine.output)
 
 print('*** part 1:', result)
 
+if data[0] == 'Register A: 729':
+    exit()
 
+# Specific to my input...
+def run_py(A):
+    B = C = 0
+    result = []
+    while True:
+        B = (A % 8) ^ 5
 
+        C = A // (2 ** B)
+        A = A // 8
+        B = B ^ C ^ 6
 
-print('*** part 2:', ...)
+        result.append(B % 8)
+        if not A:
+            break
+    return result
+
+new_result = run_py(47719761)
+print('new:', new_result)
+assert new_result == machine.output, (new_result, machine.output)
+
+for i in range(1000):
+    print(i, oct(i), run_py(i))
+
+i = 1 << (3 * (len(program) - 1))
+assert len(run_py(i-1)) < len(program)
+assert len(run_py(i)) == len(program)
+for n in range(i, i+10):
+    result = run_py(n)
+    print(n, oct(n), len(result), result)
+
+print()
+increment = i
+n = increment
+while True:
+    result = run_py(n)
+    assert len(result) == len(program)
+    for i, (a, b) in enumerate(reversed(list(zip(result, program)))):
+        if a != b:
+            break
+    else:
+        break
+    print(n, oct(n), len(result), result[:i], result[i:], n)
+    increment = 1 << ((len(program)-i-1) * 3)
+    print(i, a, b, oct(increment))
+    n += increment
+
+print(len(program), program)
+
+print('*** part 2:', n)
