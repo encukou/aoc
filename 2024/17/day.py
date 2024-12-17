afbd628221737b4f92fb466d0558a3cd5403bcea
Petr Viktorin <encukou@gmail.com>
2024-12-17 06:38:09 +0100

    2024 day 17 part 1 (simplify API)

--- 2024/17/day.py
+++ 2024/17/day.py
@@ -13,9 +13,36 @@ class Register(enum.Enum):
 
 @dataclasses.dataclass
 class Machine:
-    registers: dict
     program: list
-    output: list = dataclasses.field(default_factory=list)
+    registers: dict
+    output: list
+
+    def __init__(self, program, registers=None, **kwargs):
+        self.program = program
+        self.registers = registers or {}
+        for key, val in kwargs.items():
+            self.registers[Register[key]] = val
+        self.output = []
+
+    # machine.A  <==> machine.registers[Register.A]
+
+    def __getattr__(self, key):
+        return self.registers[Register[key]]
+
+    def __setattr__(self, key, val):
+        if len(key) == 1:
+            self.registers[Register[key]] = val
+        else:
+            super().__setattr__(key, val)
+
+    # machine[4]  <==> machine.registers[Register(4)]
+
+    def __getitem__(self, key):
+        return self.registers[Register(key)]
+
+    def __setitem__(self, key, val):
+        self.registers[Register(key)] = val
+
 
 def run(machine):
     ps = 0
@@ -30,38 +57,39 @@ def run(machine):
         def combo_op():
             if op < 4:
                 return op
-            return machine.registers[Register(op)]
+            return machine[op]
         match instruction:
             case 0:  # adv
-                machine.registers[Register.A] = machine.registers[Register.A] // (2 ** combo_op())
+                machine.A = machine.A // (2 ** combo_op())
             case 1:  # bxl
-                machine.registers[Register.B] = machine.registers[Register.B] ^ op
+                machine.B = machine.B ^ op
             case 2:  # bst
-                machine.registers[Register.B] = combo_op() % 8
+                machine.B = combo_op() % 8
             case 3:  # jnz
-                if machine.registers[Register.A]:
+                if machine.A:
                     ps = op
                     continue
             case 4:  # bxc
-                machine.registers[Register.B] = machine.registers[Register.B] ^ machine.registers[Register.C]
+                machine.B = machine.B ^ machine.C
             case 5:  # out
                 machine.output.append(combo_op() % 8)
             case 0:  # bdv
-                machine.registers[Register.B] = machine.registers[Register.A] // (2 ** combo_op())
+                machine.B = machine.A // (2 ** combo_op())
             case 7:  # cdv
-                machine.registers[Register.C] = machine.registers[Register.A] // (2 ** combo_op())
+                machine.C = machine.A // (2 ** combo_op())
             case _:
                 raise ValueError(f'{instruction=}')
         ps += 2
     print('->', machine)
     return machine
 
-assert run(Machine({Register.C: 9}, [2, 6])).registers[Register.B] == 1
-assert run(Machine({Register.A: 10}, [5,0,5,1,5,4])).output == [0, 1, 2]
-assert run(Machine({Register.A: 2024}, [0,1,5,4,3,0])).output == [4,2,5,6,7,7,7,7,3,1,0]
-assert run(Machine({Register.A: 2024}, [0,1,5,4,3,0])).registers[Register.A] == 0
-assert run(Machine({Register.B: 29}, [1,7])).registers[Register.B] == 26
-assert run(Machine({Register.B: 2024, Register.C: 43690}, [4, 0])).registers[Register.B] == 44354
+assert run(Machine([2, 6], C=9)).B == 1
+assert run(Machine([5,0,5,1,5,4], A=10)).output == [0, 1, 2]
+example_machine = run(Machine([0,1,5,4,3,0], A=2024))
+assert example_machine.output == [4,2,5,6,7,7,7,7,3,1,0]
+assert example_machine.A == 0
+assert run(Machine([1,7], B=29)).B == 26
+assert run(Machine([4, 0], B=2024, C=43690)).B == 44354
 
 registers = {}
 for line in data:
@@ -74,7 +102,7 @@ for line in data:
 print(registers)
 print(program)
 
-machine = run(Machine(registers, program))
+machine = run(Machine(program, registers))
 
 result = ','.join(str(n) for n in machine.output)
 
