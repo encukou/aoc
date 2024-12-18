59f7cc2a052be9a69db1778c214f8ad094715f7a
Petr Viktorin <encukou@gmail.com>
2024-12-18 06:23:33 +0100

    2024 day 18 part 2 (fast)

--- 2024/18/day.py
+++ 2024/18/day.py
@@ -23,10 +23,13 @@ memspace = {(r, c) for r in range(size) for c in range(size)}
 for line in data[:time]:
     c, r = line.split(',')
     memspace.remove((int(c), int(r)))
-def draw_map(memspace):
-    for r in range(size):
-        for c in range(size):
-            if (r, c) in memspace:
+def draw_map(memspace, path=None):
+    path = set(path or ())
+    for c in range(size):
+        for r in range(size):
+            if (r, c) in path:
+                print(end='O')
+            elif (r, c) in memspace:
                 print(end='.')
             else:
                 print(end='▒')
@@ -38,34 +41,54 @@ def find_path(memspace, initial=(0, 0), target=(size-1, size-1)):
     to_visit = collections.deque([(initial, 0, None)])
     while to_visit:
         pos, steps, direction = to_visit.popleft()
-        if pos == target:
-            return steps, visited
         if pos not in memspace:
             continue
         if pos in visited:
             continue
         visited[pos] = direction
+        if pos == target:
+            return steps, visited
         r, c = pos
         for direction in DIRS.values():
             dr, dc = direction
             to_visit.append(((r+dr, c+dc), steps+1, direction))
     return None, None
 
+def reconstruct_path(visited, target=(size-1, size-1)):
+    pos = target
+    tiles = []
+    while True:
+        tiles.append(pos)
+        direction = visited[pos]
+        if direction is None:
+            tiles.reverse()
+            return tiles
+        r, c = pos
+        dr, dc = direction
+        pos = r-dr, c-dc
+
 steps, visited = find_path(memspace)
+path = reconstruct_path(visited)
+
+draw_map(memspace, path)
 
 print('*** part 1:', steps)
 
+path = set(path)
 memspace = {(r, c) for r in range(size) for c in range(size)}
 for n, line in enumerate(data):
     c, r = line.split(',')
     pos = int(c), int(r)
     memspace.remove(pos)
-    if pos in visited:
+    if pos in path:
         print(n, line, '!', flush=True)
         steps, visited = find_path(memspace)
+        if steps is None:
+            break
+        path = reconstruct_path(visited)
+        path = set(path)
+        draw_map(memspace, path)
     else:
         print(n, line)
-    if steps is None:
-        break
 
 print('*** part 2:', line)
