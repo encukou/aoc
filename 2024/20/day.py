2336baa3732a54571bdfd378d054ca3f072aff07
Petr Viktorin <encukou@gmail.com>
2024-12-20 06:57:21 +0100

    2024 day 20 part 2

--- 2024/20/day.py
+++ 2024/20/day.py
@@ -53,24 +53,28 @@ for r, line in enumerate(data):
             print(f'{sr:2}', end='')
     print()
 
-CHEATS = []
+cheat_dirs = []
 for r1, c1 in DIRS.values():
     for r2, c2 in DIRS.values():
         cheat = r1+r2, c1+c2
         if cheat != (0, 0):
-            CHEATS.append(cheat)
-print(CHEATS)
+            cheat_dirs.append(cheat)
+print(cheat_dirs)
 
-cheat_durations = collections.Counter()
-for (r, c), sr in steps_remaining.items():
-    for cr, cc in CHEATS:
-        st = steps_remaining.get((r+cr, c+cc))
-        if st is not None:
-            ps_saved = st - sr - 2
-            if ps_saved > 0:
-                print(f'{r:2}:{c:2} {cr:+1}:{cc:+1}  {sr}->{st} ({ps_saved})')
-                cheat_durations[ps_saved] += 1
+def get_cheat_durations(cheat_dirs):
+    cheat_durations = collections.Counter()
+    for (r, c), sr in steps_remaining.items():
+        for cr, cc in cheat_dirs:
+            st = steps_remaining.get((r+cr, c+cc))
+            if st is not None:
+                ps_saved = st - sr - sum([abs(cr), abs(cc)])
+                if ps_saved > 0:
+                    #print(f'{r:2}:{c:2} {cr:+1}:{cc:+1}  {sr}->{st} ({ps_saved})')
+                    cheat_durations[ps_saved] += 1
+    return cheat_durations
 
+
+cheat_durations = get_cheat_durations(cheat_dirs)
 print(sorted(cheat_durations.items()))
 
 if len(data) < 50:
@@ -91,7 +95,36 @@ answer = sum(n for d, n in cheat_durations.items() if d >= 100)
 
 print('*** part 1:', answer)
 
+long_cheats = {(0, 0)}
+for ps in range(20):
+    for r, c in set(long_cheats):
+        for dr, dc in DIRS.values():
+            long_cheats.add((r+dr, c+dc))
+long_cheats = sorted(long_cheats)
+print(long_cheats)
+
+cheat_durations = get_cheat_durations(long_cheats)
+print(sorted(cheat_durations.items()))
+
+if len(data) < 50:
+    assert {k:v for k,v in cheat_durations.items() if k >= 50} == {
+         50 : 32 ,
+         52 : 31 ,
+         54 : 29 ,
+         56 : 39 ,
+         58 : 25 ,
+         60 : 23 ,
+         62 : 20 ,
+         64 : 19 ,
+         66 : 12 ,
+         68 : 14 ,
+         70 : 12 ,
+         72 : 22 ,
+         74 : 4  ,
+         76 : 3  ,
+    }
 
 
 
-print('*** part 2:', ...)
+answer = sum(n for d, n in cheat_durations.items() if d >= 100)
+print('*** part 2:', answer)
