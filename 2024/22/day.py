6862660197cc05e54c3defa934a5967e81920882
Petr Viktorin <encukou@gmail.com>
2024-12-22 06:42:50 +0100

    2024 day 22 part 2

--- 2024/22/day.py
+++ 2024/22/day.py
@@ -1,4 +1,5 @@
 import sys
+import collections
 
 data = sys.stdin.read().splitlines()
 print(data)
@@ -52,7 +53,45 @@ for line in data:
 
 print('*** part 1:', total)
 
+if len(data) == 4:
+    data = [1, 2, 3, 2024]
 
+num = 123
+for example in """
+    3 (from 123)
+    0 (from 15887950)
+    6 (from 16495136)
+    5 (etc.)
+    4
+    4
+    6
+    4
+    4
+    2
+""".strip().splitlines():
+    assert num % 10 == int(example.strip()[0])
+    num = get_next(num)
 
+sequences = collections.Counter()
+for lineno, line in enumerate(data):
+    seen = set()
+    differences = collections.deque(maxlen=4)
+    start = int(line)
+    num = start
+    for i in range(2000):
+        next_num = get_next(num)
+        diff = next_num%10 - num%10
+        differences.append(diff)
+        if len(differences) == 4:
+            td = tuple(differences)
+            #print(next_num, diff, td)
+            if td not in seen:
+                seen.add(td)
+                sequences[td] += next_num%10
+        num = next_num
+    print(lineno, sequences.most_common(3), flush=True)
+[(best_seq, num_bananas)] = sequences.most_common(1)
+if len(data) == 4:
+    print(f'{sequences[-2,1,-1,3]=}')
 
-print('*** part 2:', ...)
+print('*** part 2:', num_bananas)
