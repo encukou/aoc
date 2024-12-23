91140a650ef85004e97e3a387173701bf34e63e8
Petr Viktorin <encukou@gmail.com>
2024-12-23 06:40:44 +0100

    2024 day 23 part 2

--- 2024/23/day.py
+++ 2024/23/day.py
@@ -1,4 +1,5 @@
 import collections
+import itertools
 import sys
 
 data = sys.stdin.read().splitlines()
@@ -10,7 +11,9 @@ for line in data:
     connections[a].add(b)
     connections[b].add(a)
 
-connections = {pc: frozenset(neighbours) for pc, neighbours in connections.items()}
+connections = {
+    pc: frozenset(neighbours) for pc, neighbours in connections.items()
+}
 
 sets_of_three = set()
 for pc0, pcs1 in connections.items():
@@ -30,7 +33,22 @@ print(sets_of_three_with_t)
 
 print('*** part 1:', len(sets_of_three_with_t))
 
+interconnected_sets = set(frozenset(line.split('-')) for line in data)
+print(interconnected_sets)
+while True:
+    new_interconnected_sets = set()
+    for pc in sorted(connections):
+        for s in interconnected_sets:
+            if s <= connections[pc]:
+                print(f'Adding {pc} to {s} ({len(interconnected_sets)}→'
+                      + f'{len(new_interconnected_sets)})')
+                new_interconnected_sets.add(s | frozenset({pc}))
+    if not new_interconnected_sets:
+        break
+    print(len(new_interconnected_sets), 'sets of size',
+          len(next(iter(new_interconnected_sets))), flush=True)
+    interconnected_sets = new_interconnected_sets
+print(interconnected_sets)
+[best_set] = interconnected_sets
 
-
-
-print('*** part 2:', ...)
+print('*** part 2:', ','.join(sorted(best_set)))
