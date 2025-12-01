57b761d24ac076c444ebfbdb6c26f916d57ee9fa
Petr Viktorin <encukou@gmail.com>
2025-12-01 06:34:16 +0100

    2025 day 01 part 2, simplify

--- 2025/01/day.py
+++ 2025/01/day.py
@@ -8,36 +8,18 @@ zeros = 0
 x434C49434B = 0
 
 for line in data:
-    print(line, '@', position)
+    print(f'{position:2}: {line}')
     direction, number = line[0], int(line[1:])
     if direction == 'L':
-        while number:
-            if position == number:
-                position = number = 0
-                x434C49434B += 1
-            elif position > number:
-                position -= number
-                number = 0
-            elif position == 0:
-                if number >= 100:
-                    number -= 100
-                    x434C49434B += 1
-                else:
-                    position -= number
-                    position %= 100
-                    number = 0
-            else:
-                number -= position
-                position = 0
-                x434C49434B += 1
-            print(position, number, '!', x434C49434B)
+        x434C49434B += number // 100
+        number %= 100
+        if position and position <= number:
+            x434C49434B += 1
+        position -= number
     else:
         position += number
-        number = 0
         x434C49434B += position // 100
-        position %= 100
-    print('!', x434C49434B)
-    print(position)
+    position %= 100
     if position == 0:
         zeros += 1
 
