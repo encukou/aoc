6315f6a13c487f3ebf5ba88633fee678515903cc
Petr Viktorin <encukou@gmail.com>
2025-12-01 06:23:57 +0100

    2025 day 01 part 2, verbose

--- 2025/01/day.py
+++ 2025/01/day.py
@@ -5,15 +5,38 @@ print(data)
 
 position = 50
 zeros = 0
+x434C49434B = 0
 
 for line in data:
-    print(line)
+    print(line, '@', position)
     direction, number = line[0], int(line[1:])
     if direction == 'L':
-        position -= number
+        while number:
+            if position == number:
+                position = number = 0
+                x434C49434B += 1
+            elif position > number:
+                position -= number
+                number = 0
+            elif position == 0:
+                if number >= 100:
+                    number -= 100
+                    x434C49434B += 1
+                else:
+                    position -= number
+                    position %= 100
+                    number = 0
+            else:
+                number -= position
+                position = 0
+                x434C49434B += 1
+            print(position, number, '!', x434C49434B)
     else:
         position += number
-    position %= 100
+        number = 0
+        x434C49434B += position // 100
+        position %= 100
+    print('!', x434C49434B)
     print(position)
     if position == 0:
         zeros += 1
@@ -24,4 +47,4 @@ print('*** part 1:', zeros)
 
 
 
-print('*** part 2:', ...)
+print('*** part 2:', x434C49434B)
