a48d0b2ef71be49dc3eeb52536dc61823c9d1ff3
Petr Viktorin <encukou@gmail.com>
2025-12-06 06:14:19 +0100

    2025 day 06 part 2

--- 2025/06/day.py
+++ 2025/06/day.py
@@ -20,7 +20,27 @@ for column in zip(*(line.split() for line in data)):
 
 print('*** part 1:', total)
 
+total = 0
+numbers = []
+for column in reversed(list(zip(*data))):
+    column = ''.join(column)
+    print(column)
+    if column.strip():
+        numbers.append(int(column[:-1]))
+        print(numbers)
+        if column[-1] != ' ':
+            operation = column[-1]
+            if operation == '+':
+                result = sum(numbers)
+            else:
+                result = math.prod(numbers)
+            numbers.clear()
+            operation = ''
+            total += result
+            print(operation, result, total)
+    else:
+        assert not numbers
 
 
 
-print('*** part 2:', ...)
+print('*** part 2:', total)
