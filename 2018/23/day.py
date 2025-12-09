3d64ca46da1ff9d3fab7b968f758c3e6e865e2e8
Petr Viktorin <encukou@gmail.com>
2025-12-09 07:36:25 +0100

    2018 day 23 part 1

---
+++ 2018/23/day.py
@@ -0,0 +1,40 @@
+import dataclasses
+import sys
+import re
+
+data = sys.stdin.read().splitlines()
+print(data)
+
+RE = re.compile(r'pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(-?\d+)')
+
+@dataclasses.dataclass
+class Bot:
+    x: int
+    y: int
+    z: int
+    r: int
+
+bots = []
+strongest = None
+for line in data:
+    bot = Bot(*(int(n) for n in RE.fullmatch(line).groups()))
+    bots.append(bot)
+    if strongest is None or bot.r > strongest.r:
+        strongest = bot
+
+n_in_range = 0
+for bot in bots:
+    dist = abs(bot.x-strongest.x)+abs(bot.y-strongest.y)+abs(bot.z-strongest.z)
+    if dist <= strongest.r:
+        print(bot, dist, 'in range', strongest.r)
+        n_in_range += 1
+    else:
+        print(bot, dist, 'out of range')
+
+
+print('*** part 1:', n_in_range)
+
+
+
+
+print('*** part 2:', ...)
